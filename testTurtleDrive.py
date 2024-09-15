47ca5fa284103a970696ec1b879477bbe552b2e0
Anshi Singh <email.anshi.s@gmail.com>
2024-09-15 12:46:20 -0700

    create run() function for testing

--- testTurtleDrive.py
+++ testTurtleDrive.py
@@ -1,26 +1,24 @@
 from TurtleDrive import *
 
-td = TurtleDrive()
-# to drive stright do - td.drive(x mm)
+
+def run():
+    td = TurtleDrive()
+    # to drive stright do - td.drive(x mm)
 
 
-print(td.get_speed())
-td.drive(200)
+    print(td.get_speed())
+    td.drive(200)
 
 
-td.set_speed(speed=300)
-td.drive(200)
+    td.set_speed(speed=300)
+    td.drive(200)
 
-print(td.get_speed())
+    print(td.get_speed())
 
-td.turn(-90)
-td.drive(200)
-td.turn(90)
-
-
+    td.turn(-90)
+    td.drive(200)
+    td.turn(90)
 
 
-'''
 if __name__ == "__main__":
-    
-'''
+    run()
