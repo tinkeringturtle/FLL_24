ff2c21c2f16da7f5b264ab5c8b6d5b17eab88092
Anshi Singh <email.anshi.s@gmail.com>
2024-09-15 11:41:39 -0700

    added speed

--- TurtleDrive.py
+++ TurtleDrive.py
@@ -7,27 +7,48 @@ from pybricks.parameters import (
  Button
 )
 
-
 WHEEL_DIAMETER = 62
 AXLE_TRACK = 112
 
+SPEED = 216
+ACCELERATION = 811
+TURN_RATE = 184
+TURN_ACCELERATION=830
 
 class TurtleDrive:
- def __init__(self, GYRO=True):
-  # Initialize both motors. In this example, the motor on the
-  # left must turn counterclockwise to make the robot go forward.
-  self.left_motor = Motor(Port.A, Direction.COUNTERCLOCKWISE)
-  self.right_motor = Motor(Port.B)
-  # Initialize the drive base. In this example, the wheel diameter is 56mm.
-  # The distance between the two wheel-ground contact points is 112mm.
-  self.drive_base = DriveBase(self.left_motor, self.right_motor, wheel_diameter=WHEEL_DIAMETER, axle_track=AXLE_TRACK)
-  
-  self.drive_base.use_gyro(GYRO)
+  def __init__(self, GYRO=True):
+    # Initialize both motors. In this example, the motor on the
+    # left must turn counterclockwise to make the robot go forward.
+    self.left_motor = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+    self.right_motor = Motor(Port.B)
+    # Initialize the drive base. In this example, the wheel diameter is 56mm.
+    # The distance between the two wheel-ground contact points is 112mm.
+    self.drive_base = DriveBase(self.left_motor, self.right_motor, wheel_diameter=WHEEL_DIAMETER, axle_track=AXLE_TRACK)
+    #self.set_speed()
+    self.drive_base.use_gyro(GYRO)
 
- def drive(self, distance=0, wait=True):
-   self.drive_base.straight(distance, then=Stop.HOLD, wait=wait)
+  """
+  drive the robot straight to spcified distance 
+  """
+  def drive(self, distance=0, wait=True):
+    self.drive_base.straight(distance, then=Stop.HOLD, wait=wait)
 
+
+  """
+  Turns the robot to an specified angle
+  positive angle turen to the right, negative angle 
+  turn to left
+  """
   def turn(self, angle, wait=True):
-    self.drive_base.turn()
+    self.drive_base.turn(angle, then=Stop.HOLD, wait=wait)
+
+  """
+  Get the speed setting in tuple(straight_speed, straing_aceel, turn_speed, turn_accel)
+  """
+  def get_speed(self):
+    return self.drive_base.settings()
+  
+  def set_speed(self, speed=SPEED, acceleration=ACCELERATION, turn_rate=TURN_RATE, turn_acceleration=TURN_ACCELERATION):
+    self.drive_base.settings(speed, acceleration, turn_rate, turn_acceleration)
 
 
--- TurtleDrive_test.py
+++ TurtleDrive_test.py
@@ -4,6 +4,20 @@ td = TurtleDrive()
 # to drive stright do - td.drive(x mm)
 
 
+print(td.get_speed())
+td.drive(200)
+
+
+td.set_speed(speed=300)
+td.drive(200)
+
+print(td.get_speed())
+
+td.turn(-90)
+td.drive(200)
+td.turn(90)
+
+
 
 
 '''
