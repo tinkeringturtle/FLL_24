75615341b6ed71b1919ff42e3dec3961536c490e
Abby Pernick <abigailmpernick@gmail.com>
2024-09-22 14:40:58 -0700

    testing uploads 9/22 Chaos

---
+++ Test_Chaos.py
@@ -0,0 +1,19 @@
+from TurtleDrive import *
+
+
+def run():
+    td = TurtleDrive()
+    # to drive stright do - td.drive(x mm)
+    td.drive(100)
+
+
+def turn():
+    td = TurtleDrive()
+    td.drive(325)
+    print(td.get_speed)
+    td.set_speed(turn_rate=125, turn_acceleration=75)
+    td.turn(90)
+
+
+# run()
+turn()
