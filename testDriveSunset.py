ccf079ac767d10520fece9b8e7d63c9b164b1978
Meghana Mudunuri <rsnmeghana@gmail.com>
2024-09-22 14:37:28 -0700

    9/22/24

--- testDriveSunset.py
+++ testDriveSunset.py
@@ -7,7 +7,7 @@ def run():
     # to drive stright do - td.drive(x mm)
     td.set_speed(turn_rate=100, turn_acceleration=350)  # for sunset
     td.drive(100)
-    td.turn(90)
+    td.turn(20)
 
 
 run()
