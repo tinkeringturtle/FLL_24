d644f3f8a7685e8508a87e6e7eb9a6c35edbb0b1
Tinkering Turtle <mail.tinkering.turtle@gmail.com>
2024-09-28 13:53:34 -0700

    adding attacment

---
+++ TurtleAttachement.py
@@ -0,0 +1,49 @@
+from pybricks.pupdevices import Motor
+from pybricks.parameters import Port, Direction
+from pybricks.robotics import DriveBase
+from pybricks.parameters import Stop, Color, Button
+from pybricks.tools import wait
+from TurtleConstant import *
+from TurtleHelper import *
+
+
+class TurtleAttachment:
+    def __init__(self, ENABLE_GYRO=True, ENABLE_LOGS=False):
+        self.left = Motor(Port.C)
+        self.right = Motor(Port.D)
+
+    def move_left_angle(
+        self,
+        angle=0,
+        speed_percentage=DEFAULT_ATTACHEMNET_SPEED_PERCENTAGE,
+        then=Stop.HOLD,
+        wait=True,
+    ):
+        speed = get_speed_mmsec(speed_percentage)
+        print("Left Speed = {}".format(self.left.speed()))
+        self.left.run_angle(speed, angle, then, wait)
+        print("Left Speed = {}".format(self.left.speed()))
+
+    def move_left_time(
+        self, speed_percentage=DEFAULT_ATTACHEMNET_SPEED_PERCENTAGE, time_millisec=500
+    ):
+        speed = get_speed_mmsec(speed_percentage)
+        self.left.run(speed)
+        wait(time_millisec)
+
+    def move_right_angle(
+        self,
+        angle=0,
+        speed_percentage=DEFAULT_ATTACHEMNET_SPEED_PERCENTAGE,
+        then=Stop.HOLD,
+        wait=True,
+    ):
+        speed = get_speed_mmsec(speed_percentage)
+        self.right.run_angle(speed, angle, then, wait)
+
+    def move_right_time(
+        self, speed_percentage=DEFAULT_ATTACHEMNET_SPEED_PERCENTAGE, time_millisec=500
+    ):
+        speed = get_speed_mmsec(speed_percentage)
+        self.right.run(speed)
+        wait(time_millisec)
