7b9f598a1f936618f86b7a9bc4278e071dd507b4
Jessica Brown <jessica@brownjar.com>
2024-10-06 15:12:05 -0700

    Coral Changes for Crush

--- run_coral.py
+++ run_coral.py
@@ -1,21 +1,27 @@
-# Nitya, Page
+# Author: Nitya, Page
 
 from TurtleDrive import *
+from TurtleAttachement import *
 
 
-def runCoral():
-    td = TurtleDrive()
-    # to drive stright do - td.drive(x mm)
-    td.straight_drive(100)
+async def runAttachemnt(ta):
+    await ta.move_D_angle(angle=160, speed_percentage=20)
+    await ta.move_D_angle(angle=-160, speed_percentage=20)
 
 
-def turn():
-    td = TurtleDrive()
-    td.straight_drive(325)
-    print(td.get_speed)
-    td.set_speed(turn_rate=125, turn_acceleration=75)
+def runCoral(td, ta):
+    td.set_speed_percentage(50)
+    td.straight_drive(660)
+    run_task(runAttachemnt(ta))
+    td.turn(-90)
+    td.straight_drive(50)
+    td.straight_drive(-10)
     td.turn(90)
 
 
-runCoral()
-turn()
+if __name__ == "__main__":
+    td = TurtleDrive()
+    ta = TurtleAttachment()
+    runCoral(td, ta)
+    # run_task(runAttachemnt(ta))
+    # run_task(runCoral(td, ta))
