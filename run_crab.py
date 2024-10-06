d23cf24510bafb6fac9429570d07a2e9958984c5
Abigail Pernick <abigailmpernick@gmail.com>
2024-10-06 14:48:49 -0700

    run_crab: unknown creature and shipping lane

--- run_crab.py
+++ run_crab.py
@@ -5,16 +5,19 @@ from TurtleDrive import *
 def runCrab():
     td = TurtleDrive()
     # to drive stright do - td.drive(x mm)
-    td.drive(100)
 
-
-def turn():
-    td = TurtleDrive()
-    td.drive(325)
-    print(td.get_speed)
-    td.set_speed(turn_rate=125, turn_acceleration=75)
+    td.curve(200,-45)
+    td.set_speed_percentage(speed_percentage=40)
+    td.straight_drive(385)
+    td.straight_drive(-250)
     td.turn(90)
+    td.straight_drive(340)
+    td.turn(20)
+    td.turn(-90)
+    td.set_speed_percentage(speed_percentage=100)
+    td.straight_drive(-350)
 
 
-# runCrab()
-turn()
+if __name__ == "__main__":
+    runCrab()
+
