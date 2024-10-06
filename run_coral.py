c552a13108dcf203dbe0b9258a5e64e7afdcd0bb
Jessica Brown <jessica@brownjar.com>
2024-10-06 15:57:31 -0700

    Coral

--- run_coral.py
+++ run_coral.py
@@ -4,17 +4,18 @@ from TurtleDrive import *
 from TurtleAttachement import *
 
 
-async def runAttachemnt(ta):
-    await ta.move_D_angle(angle=160, speed_percentage=20)
-    await ta.move_D_angle(angle=-160, speed_percentage=20)
+async def runAttachemnt(ta, angle):
+    await ta.move_D_angle(angle=angle, speed_percentage=20)
 
 
 def runCoral(td, ta):
     td.set_speed_percentage(50)
-    td.straight_drive(660)
-    run_task(runAttachemnt(ta))
+    td.straight_drive(670)
+    # ta.move_D_time(75, 100)
+    run_task(runAttachemnt(ta, -45))
     td.turn(-90)
     td.straight_drive(50)
+    run_task(runAttachemnt(ta, 30))
     td.straight_drive(-10)
     td.turn(90)
 
@@ -22,6 +23,7 @@ def runCoral(td, ta):
 if __name__ == "__main__":
     td = TurtleDrive()
     ta = TurtleAttachment()
+    # ta.move_D_time(75, 2000)
     runCoral(td, ta)
     # run_task(runAttachemnt(ta))
     # run_task(runCoral(td, ta))
