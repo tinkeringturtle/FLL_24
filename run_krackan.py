90322cddd1a381b8d42e45de9f0bcabd9fd5710f
Meghana Mudunuri <rsnmeghana@gmail.com>
2024-10-06 16:01:30 -0700

    kracken: testing

--- run_krackan.py
+++ run_krackan.py
@@ -1,3 +1,4 @@
+# Author: Meghana, Emma
 from TurtleDrive import *
 from TurtleAttachement import *
 
@@ -5,14 +6,12 @@ def run_kracken():
     td = TurtleDrive()
     ta = TurtleAttachment()
     # to drive stright do - td.drive(x mm)
-    td.drive(100)
-
-
+    #td.straight_drive(200)
+    td.set_speed_percentage(speed_percentage=40)
+    td.straight_drive(220)
+    td.curve(300,90)
+    td.set_speed_percentage(speed_percentage=5)
+    td.straight_drive(65)
 
 if __name__ == "__main__":
     run_kracken()
-
-
-
-
-
