0a731f2045374ec52477d06486cfabb9bd9c27cd
Meghana Mudunuri <rsnmeghana@gmail.com>
2024-10-06 16:35:13 -0700

    kracken:tresure delivered home succsefully:))

--- run_krackan.py
+++ run_krackan.py
@@ -16,8 +16,12 @@ def run_kracken():
     td.straight_drive(20)
     td.set_speed_percentage(speed_percentage=1, acceleration_percentage=1)
     td.straight_drive(55)
-    td.straight_drive(-65)
-
+    # returning back
+    td.straight_drive(-75)
+    td.set_speed_percentage(speed_percentage=30)
+    td.curve(-300, 90)
+    td.set_speed_percentage(speed_percentage=70)
+    td.straight_drive(-220)
     td.stop()
 
 
