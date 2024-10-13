2fa201a705864a1c42745c2c74d936b9717f8a71
Meghana Mudunuri <rsnmeghana@gmail.com>
2024-10-13 16:18:00 -0700

    boat-shark:new run, but no code..

---
+++ run_boat-shark.py
@@ -0,0 +1,21 @@
+# Author: Meghana, Emma (awesome Team)
+#
+from TurtleDrive import *
+from TurtleAttachement import *
+
+async def runAttachemnt(ta, angle):
+    await ta.move_D_angle(angle=angle, speed_percentage=10)
+
+def run_boat_shark():
+    td = TurtleDrive()
+    ta = TurtleAttachment()
+    #run starts from here
+    td.straight_drive(2)
+   
+
+    #run has ended
+if __name__ == "__main__":
+    run_boat_shark()
+   
+    
+    
