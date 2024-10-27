99d2f55d2fddd2fd0ab7488828806ef0005b7838
Jessica Brown <jessica@brownjar.com>
2024-10-27 16:06:50 -0700

    deliver

---
+++ deliver_coral_tree.py
@@ -0,0 +1,14 @@
+# Author: Nitya, Page
+
+from TurtleDrive import *
+from TurtleAttachement import *
+
+
+def Deliver_Tree(td, ta):
+
+    td.straight_drive(400)
+
+    if __name__ == "__main__":
+        td = TurtleDrive()
+    ta = TurtleAttachment()
+    Deliver_Tree(td, ta)
