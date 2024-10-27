4dc4e15cda339b4324b4a1fd45ec13077854b87e
Meghana Mudunuri <rsnmeghana@gmail.com>
2024-10-27 16:23:44 -0700

    boat-shark WE DID IT .... ITS DONE.... ITS SO SIGMA

--- run_boat-shark.py
+++ run_boat-shark.py
@@ -3,18 +3,29 @@
 from TurtleDrive import *
 from TurtleAttachement import *
 
+
 async def runAttachemnt(ta, angle):
     await ta.move_D_angle(angle=angle, speed_percentage=10)
+
 
 def run_boat_shark():
     td = TurtleDrive()
     ta = TurtleAttachment()
-    #run starts from here
-    td.straight_drive(700)
-    td.turn(-50)
-    td.straight_drive(-145)
-    #run has ended
+    # run starts from here
+    td.set_speed_percentage(50, 50)
+    td.straight_drive(710)
+    td.turn(-45)
+    td.straight_drive(-200)
+    td.set_speed_percentage(30, 50)
+    td.straight_drive(160)
+    td.set_speed_percentage(15, 10)
+    td.turn(75)
+    td.set_speed_percentage(70, 40)
+    td.straight_drive(860)
+    td.turn(25)
+    td.straight_drive(550)
+    # run has ended
 
-    
+
 if __name__ == "__main__":
     run_boat_shark()
