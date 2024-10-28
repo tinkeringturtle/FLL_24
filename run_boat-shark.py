6c8b4093b5fee3836e064e519293b948f3856692
Meghana Mudunuri <rsnmeghana@gmail.com>
2024-10-27 17:02:22 -0700

    boat-shark adding trident?

--- run_boat-shark.py
+++ run_boat-shark.py
@@ -12,18 +12,23 @@ def run_boat_shark():
     td = TurtleDrive()
     ta = TurtleAttachment()
     # run starts from here
+    # delivering shark (and(trident?)
     td.set_speed_percentage(50, 50)
-    td.straight_drive(710)
-    td.turn(-45)
-    td.straight_drive(-200)
-    td.set_speed_percentage(30, 50)
-    td.straight_drive(160)
-    td.set_speed_percentage(15, 10)
-    td.turn(75)
-    td.set_speed_percentage(70, 40)
-    td.straight_drive(860)
-    td.turn(25)
-    td.straight_drive(550)
+    td.straight_drive(720)
+    td.straight_drive(-80)
+    td.turn
+    td.straight_drive(50)
+    ta.move_C_angle(-60)
+    # getting boat
+    # td.turn(-45)
+    # td.straight_drive(-200)
+    # td.straight_drive(160)
+    # td.set_speed_percentage(15, 10)
+    # td.turn(75)
+    # td.set_speed_percentage(70, 40)
+    # td.straight_drive(860)
+    # td.turn(25)
+    # td.straight_drive(550)
     # run has ended
 
 
