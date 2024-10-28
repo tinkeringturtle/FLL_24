52a90557fd8b5c9bc9a4e3bbf7663b64b199eb74
Anshi Singh <email.anshi.s@gmail.com>
2024-10-27 17:05:24 -0700

    worked on soler

--- run_submersible.py
+++ run_submersible.py
@@ -7,22 +7,20 @@ from TurtleAttachement import *
 async def runAttachemnt(ta, angle):
     await ta.move_C_angle(angle=angle, speed_percentage=50)
 
+
 def run_submersible(td, ta):
     td.set_speed_percentage(speed_percentage=60)
     td.straight_drive(690)
     td.turn(-85)
     td.straight_drive(375)
-    td.turn(30)
+    td.curve(5, 30)
     td.straight_drive(60)
     td.set_speed_percentage(speed_percentage=100)
-    td.curve(5, -50)
+    td.curve(10, -90)
     td.straight_drive(50)
-
-
 
 
 if __name__ == "__main__":
     td = TurtleDrive()
     ta = TurtleAttachment()
     run_submersible(td, ta)
-
