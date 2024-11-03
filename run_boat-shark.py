8ea03802283f04f3bf27813ea3f663632cbbe72d
Meghana Mudunuri <rsnmeghana@gmail.com>
2024-11-03 13:54:49 -0800

    boat-shark added coral thingy

--- run_boat-shark.py
+++ run_boat-shark.py
@@ -12,6 +12,12 @@ def run_boat_shark():
     td = TurtleDrive()
     ta = TurtleAttachment()
     # run starts from here
+
+    # coral
+    td.set_speed_percentage(20, 25)
+    td.straight_drive(162)
+    td.straight_drive(-162)
+    wait(5000)
     # delivering shark (and(trident?)
     td.set_speed_percentage(50, 50)
     td.straight_drive(720)
@@ -20,15 +26,15 @@ def run_boat_shark():
     td.straight_drive(50)
     ta.move_C_angle(-60)
     # getting boat
-    # td.turn(-45)
-    # td.straight_drive(-200)
-    # td.straight_drive(160)
-    # td.set_speed_percentage(15, 10)
-    # td.turn(75)
-    # td.set_speed_percentage(70, 40)
-    # td.straight_drive(860)
-    # td.turn(25)
-    # td.straight_drive(550)
+    td.turn(-45)
+    td.straight_drive(-200)
+    td.straight_drive(160)
+    td.set_speed_percentage(15, 10)
+    td.turn(75)
+    td.set_speed_percentage(70, 40)
+    td.straight_drive(860)
+    td.turn(25)
+    td.straight_drive(550)
     # run has ended
 
 
