782fb798ea5c096be332a47a14a5e130c2cbba1c
Jessica Brown <jessica@brownjar.com>
2024-11-03 13:27:20 -0800

    hi

--- run_coral.py
+++ run_coral.py
@@ -25,8 +25,43 @@ async def runAttachemnt(ta, angle, speed):
 
 def runCoral(td, ta):
     td.set_speed_percentage(50)
-    run_task(runAttachemnt(ta,10,15))
-    td.drive(65)
+    ta.move_D_time(speed_percentage=20, time_millisec=200)  # reseting arm
+    td.straight_drive(665)  # driving to coral
+    ta.move_D_time(speed_percentage=20, time_millisec=200)
+    run_task(runAttachemnt(ta, 60, 15))  # lowering arm for jimmy
+    td.set_speed_percentage(30)
+    '''
+    td.turn(-85)  # To drive into coral to get jimmy
+    td.set_speed_percentage(50)
+    td.straight_drive(65)  # driving into mission model
+    run_task(runAttachemnt(ta, 20, 10))  # raising jimmy
+    wait(400)
+    td.straight_drive(-15)  # driving back from the coral mission
+    # starting shark
+    td.turn(90)  # turn to face shark
+    td.straight_drive(40)  # driving closer to shark
+    td.turn(-50)  # to get ready to turn into shark
+    td.set_speed_percentage(40)
+    td.straight_drive(120)  # drives into shark
+    td.straight_drive(-155)  # driving away from shark
+    td.set_speed_percentage(30)
+    # starting nursery
+    td.turn(140)  # turn to go closer to nursery
+    td.straight_drive(160)  # to be in front of nursery, not face in front
+    td.turn(-95)  # turn with face in front of nursery
+    td.set_speed_percentage(14)
+    td.straight_drive(50)  # to drive into nursery
+    run_task(runAttachemnt(ta, -35, 15))
+    ta.move_C_time(speed_percentage=20, time_millisec=500)  # to lower  arms
+    run_task(runAttachemnt(ta, 40, 15))  # bringing C arm up
+    td.set_speed_percentage(75)
+    td.straight_drive(-400)  # backing from mission model
+    td.turn(60)  # turning to get home
+    td.straight_drive(-475)  # WE GOING HOME
+    run_task(
+        runAttachemnt(ta, 80, 15)
+    )  # lifting D arm so for
+    '''
 
 if __name__ == "__main__":
     td = TurtleDrive()
