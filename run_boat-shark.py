930c00446fb286181dde4e4bb610be97d2308629
Meghana Mudunuri <rsnmeghana@gmail.com>
2024-11-11 15:02:30 -0800

    boat_shark IDK trying to connect code same as alst time

--- run_boat-shark.py
+++ run_boat-shark.py
@@ -5,7 +5,7 @@ from TurtleAttachement import *
 
 
 async def runAttachemnt(ta, angle):
-    await ta.move_D_angle(angle=angle, speed_percentage=10)
+    await ta.move_C_angle(angle=angle, speed_percentage=10)
 
 
 def run_boat_shark():
@@ -14,27 +14,27 @@ def run_boat_shark():
     # run starts from here
 
     # coral
-    td.set_speed_percentage(20, 25)
-    td.straight_drive(162)
-    td.straight_drive(-162)
-    wait(5000)
-    # delivering shark (and(trident?)
-    td.set_speed_percentage(50, 50)
-    td.straight_drive(720)
-    td.straight_drive(-80)
-    td.turn
-    td.straight_drive(50)
-    ta.move_C_angle(-60)
-    # getting boat
+    # td.set_speed_percentage(20, 25)
+    # td.straight_drive(162)
+    # .straight_drive(-162)
+    # td.brake()
+    # wait(7000)
+    td.straight_drive(800)
     td.turn(-45)
-    td.straight_drive(-200)
-    td.straight_drive(160)
-    td.set_speed_percentage(15, 10)
-    td.turn(75)
-    td.set_speed_percentage(70, 40)
-    td.straight_drive(860)
-    td.turn(25)
-    td.straight_drive(550)
+    # delivering shark (and(trident?)
+
+    # td.straight_drive(50)
+    # ta.move_C_angle(-60)
+    # getting boat
+    # td.turn(-45)
+    # td.straight_drive(-200)
+    # td.straight_drive(160)
+    # td.set_speed_percentage(15, 10)
+    # td.turn(75)
+    # td.set_speed_percentage(70, 40)
+    # td.straight_drive(860)
+    # td.turn(25)
+    # td.straight_drive(550)
     # run has ended
 
 
