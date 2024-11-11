b6f0c15f95289376cc2398ad7b7ff7dbe4b2e2da
Abigail Pernick <abigailmpernick@gmail.com>
2024-11-11 14:41:17 -0800

    coral and shark

--- run_coral.py
+++ run_coral.py
@@ -35,15 +35,17 @@ def runCoral(td, ta):
     td.straight_drive(70)  # driving into mission model
     run_task(runAttachemnt(ta, -70, 10))  # raising jimmy
     wait(400)
-    td.straight_drive(-15)  # driving back from the coral mission
+    td.straight_drive(-45)  # driving back from the coral mission
     # starting shark
-    td.turn(90)  # turn to face shark
-    td.straight_drive(40)  # driving closer to shark
+    td.turn(75)  # turn to face shark
+    td.straight_drive(65)  # driving closer to shark
     td.turn(-50)  # to get ready to turn into shark
     td.set_speed_percentage(40)
     td.straight_drive(145)  # drives into shark
     td.straight_drive(-10)  # driving away from shark
     td.set_speed_percentage(30)
+    ta.move_C_time(speed_percentage=100, time_millisec=1000)
+
 
 # lifting D arm so for
 
