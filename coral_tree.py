decd285f2ff62902bf1a6486fb9d8cd3ea2bfcc4
Jessica Brown <jessica@brownjar.com>
2024-11-17 15:02:32 -0800

    Tested coral tree

--- coral_tree.py
+++ coral_tree.py
@@ -5,11 +5,13 @@ from TurtleAttachement import *
 
 
 def CoralTree(td, ta):
-
     td.straight_drive(-10)
-    td.straight_drive(400)
-    run_task(ta.move_C_angle(angle=-50, speed_percentage=15))
-    run_task(ta.move_C_angle(angle=-100, speed_percentage=35))
+    td.straight_drive(390)
+    td.turn(20)
+    td.turn(-40)
+    td.turn(20)
+    #td.turn(-40)
+    #td.turn(20)
     td.straight_drive(-385)
 
 
