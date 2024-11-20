590dff3b0d7a01d567537ed656be6d17dae2ddc3
Meghana Mudunuri <rsnmeghana@gmail.com>
2024-11-19 19:05:31 -0800

    kracken- new code(again) working on it?

--- run_krackan.py
+++ run_krackan.py
@@ -1,4 +1,4 @@
-# Author: Meghana, Emma (awesome Team)
+# Author: Meghana, Emma, Sunny (Sigma Team)
 #
 from TurtleDrive import *
 from TurtleAttachement import *
@@ -9,24 +9,24 @@ async def runAttachemnt(ta, angle):
 
 
 def run_kracken(td, ta):
-
+    # line up is on the middile of the 4rth line (coming from left counting)
     # to drive stright do - td.drive(x mm)
     # td.straight_drive(200)
     td.set_speed_percentage(speed_percentage=40)
-    td.straight_drive(220)
-    td.curve(280, 90)
-    td.set_speed_percentage(speed_percentage=50)
-    td.straight_drive(20)
-    td.set_speed_percentage(speed_percentage=1, acceleration_percentage=1)
-    td.straight_drive(50)
-    # getting qrill
-    run_task(runAttachemnt(ta, 118))
-    # returning back
-    td.straight_drive(-90)
-    td.set_speed_percentage(speed_percentage=30)
-    td.curve(-300, 90)
-    td.set_speed_percentage(speed_percentage=70)
-    td.straight_drive(-220)
+    td.straight_drive(490)
+    td.turn(85)
+    td.straight_drive(170)
+    td.set_speed_percentage(speed_percentage=5)
+    td.straight_drive(145)
+    td.straight_drive(-45)
+    run_task(runAttachemnt(ta, 130))
+    # returning home
+    td.set_speed_percentage(speed_percentage=10)
+    td.straight_drive(-225)
+    td.set_speed_percentage(speed_percentage=0.000001)
+    td.turn(-60)
+    td.set_speed_percentage(speed_percentage=35)
+    td.straight_drive(-480)
     run_task(runAttachemnt(ta, -118))
     td.stop()
 
