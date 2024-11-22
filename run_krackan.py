902ffc295e6579cfa78175aff73c0ff567131bd2
Meghana Mudunuri <rsnmeghana@gmail.com>
2024-11-21 18:39:26 -0800

    kracken- new code, wirking?!?

--- run_krackan.py
+++ run_krackan.py
@@ -12,6 +12,15 @@ def run_kracken(td, ta):
     # line up is on the middile of the 4rth line (coming from left counting)
     # to drive stright do - td.drive(x mm)
     # td.straight_drive(200)
+    # Author: Meghana, Emma, Sunny (Sigma Team)
+    # from TurtleDrive import *from TurtleAttachement import *
+
+    async def runAttachemnt(ta, angle):
+        await ta.move_D_angle(angle=angle, speed_percentage=10)
+
+
+def run_kracken(td, ta):
+    # line up is on the middile of the 4rth line (coming from left counting)
     td.set_speed_percentage(speed_percentage=40)
     td.straight_drive(80)
     td.turn(10)
@@ -20,14 +29,23 @@ def run_kracken(td, ta):
     td.set_speed_percentage(speed_percentage=10)
     td.straight_drive(125)
     td.set_speed_percentage(speed_percentage=2)
-    td.straight_drive(100)
+    td.straight_drive(85)
     td.straight_drive(-45)
-    run_task(runAttachemnt(ta, 130))
+    run_task(runAttachemnt(ta, 118))
     # returning home
-    td.set_speed_percentage(speed_percentage=10)
-    td.curve(-690, 23)
+    td.set_speed_percentage(speed_percentage=20)
+    td.straight_drive(-225)
+    td.turn(-60)
+    td.set_speed_percentage(speed_percentage=60)
+    td.straight_drive(-520)
     run_task(runAttachemnt(ta, -118))
     td.stop()
+
+
+if __name__ == "__main__":
+    td = TurtleDrive()
+    ta = TurtleAttachment()
+    run_kracken(td, ta)
 
 
 if __name__ == "__main__":
