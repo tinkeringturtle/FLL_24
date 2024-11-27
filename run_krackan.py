c9d7ba41e8395a71456fa823a9d7df15f09ee8e8
Meghana Mudunuri <rsnmeghana@gmail.com>
2024-11-27 14:08:59 -0800

    kracken-new thing now the chest is workign

--- run_krackan.py
+++ run_krackan.py
@@ -27,10 +27,10 @@ def run_kracken(td, ta):
     td.straight_drive(460)
     td.turn(80)
     td.set_speed_percentage(speed_percentage=10)
-    td.straight_drive(125)
+    td.straight_drive(165)
     td.set_speed_percentage(speed_percentage=2)
     td.straight_drive(85)
-    td.straight_drive(-45)
+    td.straight_drive(-30)
     run_task(runAttachemnt(ta, 118))
     # returning home
     td.set_speed_percentage(speed_percentage=20)
