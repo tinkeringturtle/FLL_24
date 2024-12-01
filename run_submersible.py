5169c91a0ce67f5a22baec02127405681f1224d6
Anshi Singh <email.anshi.s@gmail.com>
2024-12-01 12:43:32 -0800

    working on master code

--- run_submersible.py
+++ run_submersible.py
@@ -1,5 +1,6 @@
 # Anshi, Alice
-
+from pybricks.hubs import PrimeHub
+from pybricks.parameters import Button, Icon
 from TurtleDrive import *
 from TurtleAttachement import *
 
@@ -11,6 +12,7 @@ async def runAttachemnt(ta, angle):
 def run_submersible(td, ta):
     td.set_speed_percentage(speed_percentage=60)
     td.straight_drive(500)
+    # <<<<<<< HEAD
     td.turn(-47)
     td.straight_drive(200)
     # td.turn(-85)
@@ -32,7 +34,33 @@ def run_submersible(td, ta):
     # td.straight_drive(157)
     # run_task(runAttachemnt(ta, -450))
     # wait(1000)
+    # td.straight_drive(-60)=
+    td.turn(-50)
+    td.straight_drive(650)
+    td.straight_drive(-150)
+    td.turn(-35)
+    td.straight_drive(70)
+    td.turn(20)
+    td.straight_drive(80)
+    run_task(runAttachemnt(ta, -450))
+    wait(1050)
+    td.straight_drive(-80)
+    td.turn(65)
+    td.straight_drive(-60)
+    # td.turn(30)
+    # td.straight_drive(80)
+    # td.turn(-90)
+    # td.straight_drive(30)
+    # td.turn(90)
+    # td.straight_drive(-150)
+    # td.turn(-35)
+    # td.straight_drive(157)
+    # run_task(runAttachemnt(ta,-450))
+    # wait(1000)
     # td.straight_drive(-60)
+
+
+# >>>>>>> cc63350 (working on master code)
 
 
 if __name__ == "__main__":
