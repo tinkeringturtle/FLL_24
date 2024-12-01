1090db725efaeb8afc6260d778e6bc30d18f2e95
Meghana Mudunuri <rsnmeghana@gmail.com>
2024-12-01 15:23:01 -0800

    kracken-new codes

--- run_krackan.py
+++ run_krackan.py
@@ -21,24 +21,23 @@ def run_kracken(td, ta):
 
 def run_kracken(td, ta):
     # line up is on the middile of the 4rth line (coming from left counting)
-    td.set_speed_percentage(speed_percentage=40)
+    td.set_speed_percentage(speed_percentage=40, acceleration_percentage=35)
     td.straight_drive(80)
     td.turn(10)
     td.straight_drive(460)
     td.turn(80)
-    td.set_speed_percentage(speed_percentage=10)
+    td.set_speed_percentage(speed_percentage=10, acceleration_percentage=10)
     td.straight_drive(125)
-    td.set_speed_percentage(speed_percentage=2)
-    td.straight_drive(85)
+    td.set_speed_percentage(speed_percentage=5, acceleration_percentage=5)
+    td.straight_drive(82)
     td.straight_drive(-45)
     run_task(runAttachemnt(ta, 118))
     # returning home
-    td.set_speed_percentage(speed_percentage=20)
+    td.set_speed_percentage(speed_percentage=20, acceleration_percentage=15)
     td.straight_drive(-225)
     td.turn(-60)
-    td.set_speed_percentage(speed_percentage=60)
+    td.set_speed_percentage(speed_percentage=65, acceleration_percentage=60)
     td.straight_drive(-520)
-    run_task(runAttachemnt(ta, -118))
     td.stop()
 
 
