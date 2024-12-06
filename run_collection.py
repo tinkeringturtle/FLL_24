259885486758bfcbdbac0fc3cf932b4e4d4d9134
Meghana Mudunuri <rsnmeghana@gmail.com>
2024-12-05 16:38:30 -0800

    Collection = 100%

--- run_collection.py
+++ run_collection.py
@@ -17,19 +17,6 @@ def run_Krillies(td, ta):
     td.set_speed_percentage(60)
     td.straight_drive(410)  # collect octopus
     td.straight_drive(-340)
-
-    """
-    td.turn(30)
-    td.straight_drive(175)
-    td.turn(40)
-    td.straight_drive(125)
-    td.turn(-45)
-    td.straight_drive(130)
-    td.turn(12)
-    td.turn(60)
-    td.straight_drive(250)
-    """
-
     td.set_speed_percentage(50)
     td.curve(100, 45)
     td.straight_drive(320)
@@ -38,24 +25,10 @@ def run_Krillies(td, ta):
     run_task(ta.move_D_angle(150))
     td.set_speed_percentage(50)
     td.curve(-200, 80)
-    td.curve(470, -53)
+    td.curve(500, -53)
     td.straight_drive(630)  # drive past angler fish
     run_task(ta.move_D_angle(-150))
     td.straight_drive(50)
-
-    """
-
-    td.set_speed_percentage(75)
-    td.straight_drive(250)
-    td.turn(-20)
-    td.straight_drive(95)
-    td.turn(10)
-    td.straight_drive(60)
-    td.turn(15)
-    td.straight_drive(300)
-    run_task(ta.move_D_angle(-90))
-    td.set_speed_percentage(50)
-    """
     td.curve(400, -45)
     td.turn(-45)
     td.straight_drive(200)
