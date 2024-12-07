d826f6ae72f665233b32dbd48d07d3edf3fbe7b3
Meghana Mudunuri <rsnmeghana@gmail.com>
2024-12-07 06:55:55 -0800

    boat-shark dang it

--- run_boat_shark2.py
+++ run_boat_shark2.py
@@ -12,32 +12,32 @@ def run_boat_shark2(td, ta):
     # boat/shark
 
     td.set_speed_percentage(50, 45)
-    td.curve(1100, -40)
+    td.curve(1090, -40)
     td.straight_drive(-150)
-    td.turn(45)
+    td.turn(46)
     # crab
     td.straight_drive(420)
     run_task(runAttachemnt(ta, 110))
-    td.straight_drive(-190)
-    run_task(runAttachemnt(ta, -2))
-    td.straight_drive(-30)
-    run_task(runAttachemnt(ta, -109))
+    td.straight_drive(-220)
+    run_task(runAttachemnt(ta, -1))
+    td.straight_drive(-50)
+    run_task(runAttachemnt(ta, -110))
     # boat
     td.turn(-55)
-    td.straight_drive(-180)
+    td.straight_drive(-175)
     # going to other side
     td.straight_drive(130)
     td.set_speed_percentage(15, 10)
-    td.turn(20)
+    td.turn(17)
     td.set_speed_percentage(25, 10)
-    td.straight_drive(100)
+    td.straight_drive(60)
     td.set_speed_percentage(50, 50)
     td.straight_drive(100)
     td.turn(25)
     td.set_speed_percentage(70, 70)
     td.straight_drive(485)
-    td.turn(50)
-    td.straight_drive(1100)
+    td.turn(45)
+    td.straight_drive(1200)
 
 
 """
