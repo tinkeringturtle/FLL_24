3463dceb86335ca1a82588e951359f18e8b4efc1
Meghana Mudunuri <rsnmeghana@gmail.com>
2024-12-07 11:23:02 -0800

    collection

--- run_collection.py
+++ run_collection.py
@@ -15,18 +15,18 @@ def run_Krillies(td, ta):
     td.set_speed_percentage(50)
     td.curve(200, -45)
     td.set_speed_percentage(60)
-    td.straight_drive(410)  # collect octopus
+    td.straight_drive(435)  # collect octopus
     td.straight_drive(-340)
     td.set_speed_percentage(50)
     td.curve(100, 45)
-    td.straight_drive(350) #adjust length to get past banana boat
+    td.straight_drive(350)  # adjust length to get past banana boat
     td.curve(100, 45)
     td.straight_drive(180)  # collect seaweed
     run_task(ta.move_D_angle(150))
     td.set_speed_percentage(50)
-    td.curve(-230, 80) #adjust radius to go around banana boat
-    td.curve(540, -53) #adjust radium to get closer or further from angular fish)
-    td.straight_drive(630)  # drive past angler fish
+    td.curve(-230, 80)  # adjust radius to go around banana boat
+    td.curve(525, -53)  # adjust radium to get closer or further from angular fish)
+    td.straight_drive(640)  # drive past angler fish
     run_task(ta.move_D_angle(-150))
     td.straight_drive(50)
     td.curve(400, -45)
