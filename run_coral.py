db1d9fdc018d08b2e6c8fe860222e1e3ec156287
Meghana Mudunuri <rsnmeghana@gmail.com>
2024-12-17 17:11:23 -0800

    coral- omg sigma chat

--- run_coral.py
+++ run_coral.py
@@ -28,14 +28,14 @@ async def runAttachemntC(ta, angle, speed):
 
 
 def runCoral(td, ta):
-    td.set_speed_percentage(50)
+    td.set_speed_percentage(75)
     ta.move_D_time(speed_percentage=20, time_millisec=200)  # reseting arm
-    td.straight_drive(635)  # driving to coral
+    td.straight_drive(655)  # driving to coral
     ta.move_D_time(speed_percentage=20, time_millisec=200)
     run_task(runAttachemnt(ta, 60, 15))  # lowering arm for jimmy
-    td.set_speed_percentage(30)
+    td.set_speed_percentage(60)
     td.turn(-85)  # To drive into coral to get jimmy
-    td.set_speed_percentage(50)
+    td.set_speed_percentage(70)
     td.straight_drive(63)  # driving into mission model
     wait(30)
     run_task(runAttachemnt(ta, -100, 10))  # raising jimmy
@@ -45,19 +45,15 @@ def runCoral(td, ta):
     td.turn(85)  # turn to face shark
     td.straight_drive(80)  # driving closer to shark
     td.turn(-50)  # to get ready to turn into shark
-    td.set_speed_percentage(40)
+    td.set_speed_percentage(55)
     td.straight_drive(155)  # drives into shark
     td.straight_drive(-5)  # driving away from shark
     td.turn(-10)
-    td.set_speed_percentage(30)
-    ta.move_C_angle()
-    # ta.move_C_time(speed_percentage=100, time_millisec=2000)
-    run_task(runAttachemntC(ta, -500, 100))
-    run_task(runAttachemntC(ta, 500, 100))
-    td.straight_drive(-100)
-    td.turn(70)
-    td.set_speed_percentage(130)
-    td.straight_drive(-745)
+    td.set_speed_percentage(90)
+    td.straight_drive(-130)
+    td.turn(59)
+    td.set_speed_percentage(100)
+    td.straight_drive(-750)
 
 
 # lifting D arm so for
