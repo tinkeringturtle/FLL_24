53919f31412bc0e31d200fa162d8e0f16c9746bb
Meghana Mudunuri <rsnmeghana@gmail.com>
2024-12-20 15:35:16 -0800

    collection dec/20/24

--- run_collection.py
+++ run_collection.py
@@ -11,7 +11,7 @@ hub = PrimeHub()
 
 
 def run_Krillies(td, ta):
-    run_task(ta.move_C_angle(150))
+    run_task(ta.move_C_angle(140))
     td.set_speed_percentage(100)
     td.curve(200, -45)
     td.set_speed_percentage(50)
@@ -22,25 +22,28 @@ def run_Krillies(td, ta):
     td.curve(100, 45)
     td.straight_drive(330)  # adjust length to get past banana boat
     td.curve(100, 45)
-    td.straight_drive(140)  # collect seaweed
-    run_task(ta.move_C_angle(-150))
+    td.straight_drive(150)  # collect seaweed
+    run_task(ta.move_C_angle(-140))
+    run_task(ta.move_D_angle(250))
     td.set_speed_percentage(50)
     td.curve(-230, 80)  # adjust radius to go around banana boat
-    td.curve(670, -57)  # adjust radium to get closer or further from angular fish)
-    td.straight_drive(275)  # drive past angler fish
-    run_task(ta.move_D_angle(1000))
+    td.curve(650, -57)  # adjust radium to get closer or further from angular fish)
+    td.straight_drive(260)  # drive past angler fish
+    run_task(ta.move_D_angle(520))
+    """
     td.set_speed_percentage(50, 50)
     td.straight_drive(120)
-    run_task(ta.move_D_angle(-1000))
-    run_task(ta.move_C_angle(150))
+    run_task(ta.move_D_angle(-520))
+    run_task(ta.move_C_angle(140))
     td.straight_drive(100)
     td.curve(400, -45)
     td.turn(-45)
     td.straight_drive(200)
     td.curve(500, 50)
-    # td.straight_drive(620)  # drive past angler fish
-    # run_task(ta.move_D_angle(-150))
-    # td.straight_drive(50)
+    td.straight_drive(620)  # drive past angler fish
+    run_task(ta.move_D_angle(-150))
+    td.straight_drive(50)
+    """
 
 
 # td.curve(400, -45)
