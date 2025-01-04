e4974e39561299f8e2bb4e59566b1db4a9284cb2
Meghana Mudunuri <rsnmeghana@gmail.com>
2025-01-04 12:18:24 -0800

    collection- working for now

--- run_collection.py
+++ run_collection.py
@@ -22,28 +22,42 @@ def run_Krillies(td, ta):
     td.set_speed_percentage(50)
     td.curve(110, 45)
     td.straight_drive(330)  # adjust length to get past banana boat
-    td.curve(100, 55)
-    td.straight_drive(150)  # collect seaweed
+    td.curve(120, 50)
+    td.straight_drive(120)  # collect seaweed
     run_task(ta.move_C_angle(-140))
     td.set_speed_percentage(50)
-    td.curve(-230, 80)  # adjust radius to go around banana boat
+    td.curve(-230, 81)  # adjust radius to go around banana boat
     run_task(ta.move_D_angle(250))
-    td.curve(460, -57)  # adjust radium to get closer or further from angular fish)
+    td.curve(470, -57)  # adjust radium to get closer or further from angular fish)
     run_task(ta.move_D_angle(-250))
-    td.straight_drive(495)  # drive past angler fish and sample adjust for reach
+    td.straight_drive(475)  # drive past angler fish and sample adjust for reach
     run_task(ta.move_D_angle(715))
     td.set_speed_percentage(50, 50)
     run_task(ta.move_C_angle(140))
-    td.straight_drive(105)
+    td.straight_drive(170)
     run_task(ta.move_D_angle(-700))
-    td.turn(-6)
-    td.straight_drive(345)  # getting krill and other sample
-    td.turn(-75)
-    td.straight_drive(60)
-    td.straight_drive(135)
-    # td.turn(35)
+    td.turn(-15)
+    td.straight_drive(300)  # getting krill and other sample number 3
+    td.turn(-40)
+    td.straight_drive(50)
+    run_task(ta.move_C_angle(-140))
+    wait(1000)
+    run_task(ta.move_C_angle(140))
+    td.turn(-50)
     td.set_speed_percentage(100)
-    td.straight_drive(600)
+    td.straight_drive(100)
+    td.turn(40)
+    td.set_speed_percentage(1000)
+    td.straight_drive(450)
+
+    # td.straight_drive(100)
+
+    # td.straight_drive(60)
+    # put a left turn here later if wanted
+    # td.straight_drive(135)
+    # td.turn(35)
+    # td.set_speed_percentage(100)
+    # td.straight_drive(600)
 
     """
     td.straight_drive(100)
