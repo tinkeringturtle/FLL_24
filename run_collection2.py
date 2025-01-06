fdafe796c098e4ccb473ff58551bb8a7f422bef2
Abigail Pernick <abigailmpernick@gmail.com>
2025-01-05 17:03:25 -0800

    almost done :)

--- run_collection2.py
+++ run_collection2.py
@@ -13,22 +13,22 @@ from test_run import *
 
 def run_Krillies(td, ta):
     # run_task(ta.move_C_angle(140))
-  #  ta.move_C_angle_sync(140)
-    td.set_speed_percentage(100,90)
+    #  ta.move_C_angle_sync(140)
+    td.set_speed_percentage(100, 90)
     td.curve(200, -45)
-    td.set_speed_percentage(50,45)
+    td.set_speed_percentage(50, 45)
     td.straight_drive(410)  # collect octopus
     td.straight_drive(-340)
-    td.set_speed_percentage(50,45)
+    td.set_speed_percentage(50, 45)
     td.curve(110, 45)
     td.straight_drive(330)  # adjust length to get past banana boat
     td.curve(120, 50)
     td.straight_drive(150)  # collect seaweed
     # run_task(ta.move_C_angle(-140))
     ta.move_C_angle_sync(-140)
-    td.set_speed_percentage(50,45)
+    td.set_speed_percentage(50, 45)
     td.curve(-230, 81)  # adjust radius to go around banana boat
-    td.set_speed_percentage(100,90)
+    td.set_speed_percentage(100, 90)
     td.straight_drive(-500)
 
 
@@ -41,9 +41,11 @@ def run_anglerfish(td, ta):
     td.turn(-70)
     run_task(ta.move_C_angle(150))
     td.straight_drive(450)
-    td.turn(-20)
+    td.turn(-40)
     td.straight_drive(50)
-   
+    td.turn(-13)
+    td.set_speed_percentage(100, 90)
+    td.straight_drive(700)
 
 
 if __name__ == "__main__":
