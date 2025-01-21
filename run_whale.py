c5458bf1e8c6b243f442760387a0645ccbad76d3
Meghana Mudunuri <rsnmeghana@gmail.com>
2025-01-20 17:09:00 -0800

    fixed the yellow boat

--- run_whale.py
+++ run_whale.py
@@ -11,7 +11,7 @@ async def runAttachemnt(ta, angle):
 # line nine
 def run_whale(td, ta):
     td.set_speed_percentage(speed_percentage=80)
-    td.straight_drive(400)
+    td.straight_drive(395)
     td.turn(20)
     td.straight_drive(65)
     td.set_speed_percentage(speed_percentage=75)
