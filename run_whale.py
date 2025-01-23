7f8cac60ee7f57a9aaed950e02ca139c7be1ac50
Meghana Mudunuri <rsnmeghana@gmail.com>
2025-01-22 17:53:34 -0800

    Fixed the yellow boat

--- run_whale.py
+++ run_whale.py
@@ -1,5 +1,7 @@
 # Anshi, Alice
 
+# Anshi, Alice
+
 from TurtleDrive import *
 from TurtleAttachement import *
 
@@ -12,32 +14,31 @@ async def runAttachemnt(ta, angle):
 def run_whale(td, ta):
     td.set_speed_percentage(speed_percentage=80)
     td.turn(-10)
-    td.straight_drive(60)
-    # td.straight_drive(375)
-    td.turn(20)
-    td.straight_drive(85)
+    td.straight_drive(395)
+    td.turn(45)
+    td.straight_drive(150)
     td.set_speed_percentage(speed_percentage=75)
     td.turn(45)
     td.turn(-65)
     td.set_speed_percentage(speed_percentage=60)
-    td.straight_drive(285)
-    td.set_speed_percentage(speed_percentage=15)
-    td.turn(40)
+    td.straight_drive(170)
+    td.set_speed_percentage(speed_percentage=25)
+    td.turn(15)
     td.straight_drive(160)
     run_task(runAttachemnt(ta, -50))
     td.stop()
     td.set_speed_percentage(speed_percentage=60)
-    td.straight_drive(-200)
+    td.straight_drive(-250)
     td.set_speed_percentage(speed_percentage=10)
-    td.turn(130)
+    td.turn(140)
     td.set_speed_percentage(speed_percentage=39)
-    td.straight_drive(-270)
+    td.straight_drive(-345)
     td.set_speed_percentage(speed_percentage=5)
     td.turn(30)
     td.turn(-30)
     td.set_speed_percentage(speed_percentage=80)
     td.straight_drive(540)
-    td.curve(500, -54)
+    td.curve(430, -45)
 
 
 if __name__ == "__main__":
