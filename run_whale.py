0b4683dde4acc832af2a5ac723acd684e5acd0f4
Meghana Mudunuri <rsnmeghana@gmail.com>
2025-01-24 15:41:19 -0800

    fixed whale

--- run_whale.py
+++ run_whale.py
@@ -12,29 +12,29 @@ async def runAttachemnt(ta, angle):
 
 # line nine
 def run_whale(td, ta):
-    td.set_speed_percentage(80, 75)
+    td.set_speed_percentage(85, 80)
     td.turn(-10)
     td.straight_drive(395)
     td.turn(45)
     td.straight_drive(150)  # Aproaches boat
-    td.set_speed_percentage(75, 70)
+    td.set_speed_percentage(80, 75)
     td.turn(45)
     td.turn(-65)  # flip yellow boat
-    td.set_speed_percentage(60, 55)
+    td.set_speed_percentage(65, 60)
     td.straight_drive(170)
-    td.set_speed_percentage(25, 20)
+    td.set_speed_percentage(30, 25)
     td.turn(15)
     td.straight_drive(160)  # open whale mouth
     run_task(runAttachemnt(ta, -50))  # drop krill in whale mouth
     td.stop()
-    td.set_speed_percentage(60, 55)
+    td.set_speed_percentage(65, 60)
     td.straight_drive(-250)
-    td.set_speed_percentage(10, 5)
+    td.set_speed_percentage(15, 10)
     td.turn(140)
-    td.set_speed_percentage(39, 34)
-    td.straight_drive(-345)
+    td.set_speed_percentage(44, 39)
+    td.straight_drive(-355)
     td.set_speed_percentage(10, 5)
-    td.turn(30)  # go behind the sonar
+    td.turn(35)  # go behind the sonar
     td.turn(-30)
     td.set_speed_percentage(100, 95)
     td.straight_drive(540)  # Flip the first sonar
