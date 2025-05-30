fe461a9844dfafcb88d3ed9da0b299b3e4aa956c
Anand Singh <anand.krs@gmail.com>
2025-05-30 15:33:46 -0700

    a better script to program the robot

---
+++ .vscode/runRobot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+#source ../robotName.sh
+echo ">> Executing: $0"
+echo ">> RobotName: $1"
+echo ">> File: $2"
+robotName=$(cat $1)
+echo "Robot Name is ? $robotName"
+
+filename=$(basename "$2")
+if [[ "${filename##*.}" != "py" ]]; then
+  echo "Not a Python file, so exiting !!!"
+else
+    echo "Loading $2 on $robotName"
+    pybricksdev run ble --name $robotName $2
+fi
