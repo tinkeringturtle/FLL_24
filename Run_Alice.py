5b5fe9186d56d9bd5c8497eb633b58d597ec3d42
Alice Hunter <emma.yasuda@gmail.com>
2025-06-06 14:51:05 -0700

    Alice file pasting

--- Run_Alice.py
+++ Run_Alice.py
@@ -1,1 +1,15 @@
-#Alice
+from TurtleDrive import *
+from TurtleAttachement import *
+
+
+# line nine
+def run_Alice(td, ta):
+    print("start run")
+    td.straight_drive(500)
+
+
+if __name__ == "__main__":
+    print("Hello")
+    td = TurtleDrive()
+    ta = TurtleAttachment()
+    run_anand(td, ta)
