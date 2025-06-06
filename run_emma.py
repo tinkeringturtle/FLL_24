f9dab39d27cd35e24dde3d0bae4acc22c675ec4a
Emma Hunter <emma.yasuda@gmail.com>
2025-06-06 14:38:39 -0700

    drive striat stuff

---
+++ run_emma.py
@@ -0,0 +1,15 @@
+from TurtleDrive import *
+from TurtleAttachement import *
+
+
+# line nine
+def run_emma(td, ta):
+    print("start run")
+    td.straight_drive(500)
+
+
+if __name__ == "__main__":
+    print("Hello")
+    td = TurtleDrive()
+    ta = TurtleAttachment()
+    run_emma(td, ta)
