6a6e22ed938ee521117e339a1f08e81ab18c9bbc
Paige Kennedy <kennedy.paige.marie@gmail.com>
2025-06-06 14:38:38 -0700

    test sunset code

--- Run_Paige.py
+++ Run_Paige.py
@@ -1,3 +1,15 @@
+from TurtleDrive import *
+from TurtleAttachement import *
+
+
+# line nine
 def Run_Paige(td, ta):
     print("start run")
     td.straight_drive(500)
+
+
+if __name__ == "__main__":
+    print("Hello")
+    td = TurtleDrive()
+    ta = TurtleAttachment()
+    run_anand(td, ta)
