50659043b13cddfeb8b0f528ae059328aeccec13
Jessica Brown <jgb.shanghai@gmail.com>
2025-06-06 14:42:06 -0700

    tested robot

--- run_Jessica.py
+++ run_Jessica.py
@@ -1,5 +1,13 @@
 from TurtleDrive import *
 from TurtleAttachement import *
 
-def run_anand(td, ta):
-    
+def run_Jessica(td, ta):
+    print("start run")
+    td.straight_drive(500)
+
+if __name__ == "__main__":
+    print("hello")
+    td = TurtleDrive()
+    ta = TurtleAttachment()
+    run_Jessica(td, ta)
+
