2c0154559025b4a496f0246fbdbdce5b0effe236
Abigail Pernick <abigailmpernick@gmail.com>
2025-06-06 14:42:46 -0700

    inserted drive

--- Run_Abby.py
+++ Run_Abby.py
@@ -1,1 +1,14 @@
-#hi
+from TurtleDrive import *
+from TurtleAttachement import *
+
+# line nine
+def run_anand(td, ta):
+    print("start run")
+    td.straight_drive(500)
+
+
+if __name__ == "__main__":
+    print("Hello")
+    td = TurtleDrive()
+    ta = TurtleAttachment()
+    run_anand(td, ta)
