ccb3ad5a8595ae4c0ee8cd9294c3ce8b81c846d5
Alice Hunter <emma.yasuda@gmail.com>
2025-06-20 15:02:24 -0700

    we made the distance right, also i made anshi mad by being a complte loser

---
+++ test_flie.py
@@ -0,0 +1,15 @@
+from TurtleDrive import *
+from TurtleAttachement import *
+
+
+# line nine
+def run_Test(td, ta):
+    print("start run")
+    td.straight_drive(500)
+
+
+if __name__ == "__main__":
+    print("Hello")
+    td = TurtleDrive()
+    ta = TurtleAttachment()
+    run_Test(td, ta)
