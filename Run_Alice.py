36bf8f868bc2ff7fa1c341ffc9905db4eeecfc10
Elly Druru <elly.drury@gmail.com>
2025-06-20 16:18:02 -0700

    Made front attachment

--- Run_Alice.py
+++ Run_Alice.py
@@ -1,11 +1,16 @@
 from TurtleDrive import *
 from TurtleAttachement import *
 
+async def runAttachemnt(ta, angle):
+    await ta.move_C_angle(angle=angle, speed_percentage=40)
 
 # line nine
 def run_Alice(td, ta):
     print("start run")
-    td.straight_drive(500)
+    #td.straight_drive(500)
+    run_task(runAttachemnt(ta, 180))
+    run_task(runAttachemnt(ta, -180))
+
 
 
 if __name__ == "__main__":
