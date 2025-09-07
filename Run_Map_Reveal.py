c05f624e16036acd98844afda5c30141107bdd51
Abigail Pernick <abigailmpernick@gmail.com>
2025-09-07 12:56:20 -0700

    9/7/2025 AM started code

--- Run_Map_Reveal.py
+++ Run_Map_Reveal.py
@@ -1,14 +1,29 @@
 from TurtleDrive import *
 from TurtleAttachement import *
 
+
 # line nine
-def run_anand(td, ta):
+def run_Map_Reveal(td, ta):
     print("start run")
-    td.straight_drive(500) 
-    
+    td.set_speed_percentage(75)
+    td.straight_drive(720)
+    td.turn(-60)
+    td.set_speed_percentage(35)
+    td.straight_drive(200)
+    td.curve(-180, -75)
+    td.set_speed_percentage(50)
+    td.straight_drive(-100)
+    td.straight_drive(230)
+    td.curve(50, -60)
+    td.straight_drive(20)
+    ta.run_task(runAttachement(ta, 50))
+    td.curve(-50, 60)
+    td.set_speed_percentage(100)
+    td.straight_drive(320)
+
 
 if __name__ == "__main__":
     print("Hello")
     td = TurtleDrive()
     ta = TurtleAttachment()
-    run_anand(td, ta)   
+    run_Map_Reveal(td, ta)
