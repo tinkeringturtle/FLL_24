43fa226af28c0af0c00aad57fd6097bbb7a4e1b5
Meghana Mudunuri <rsnmeghana@gmail.com>
2025-09-07 12:58:43 -0700

    we r working on the attahcment so far no coding just empty space - meghana : 12:58 am

--- Run_Seal_Jones.py
+++ Run_Seal_Jones.py
@@ -5,16 +5,21 @@ async def runAttachemnt(ta, angle):
     await ta.move_C_angle(angle=angle, speed_percentage=40)
 
 # line nine
-def run_Alice(td, ta):
+def Run_Seal_Jones(td, ta):
     print("start run")
-    #td.straight_drive(500)
-    run_task(runAttachemnt(ta, 180))
-    run_task(runAttachemnt(ta, -180))
+
+#Run starts here
+    
+#Run ends here
 
 
+
+
+ #run_task(runAttachemnt(ta, 180))
+    #run_task(runAttachemnt(ta, -180))
 
 if __name__ == "__main__":
     print("Hello")
     td = TurtleDrive()
     ta = TurtleAttachment()
-    run_Alice(td, ta)
+    Run_Seal_Jones(td, ta)
