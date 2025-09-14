57d69ea8e920eabe2b223d2278155d850c8e71d3
Abigail Pernick <abigailmpernick@gmail.com>
2025-09-14 12:54:30 -0700

    attachment map reveal new

--- Run_Map_Reveal.py
+++ Run_Map_Reveal.py
@@ -5,11 +5,12 @@ from TurtleAttachement import *
 async def runAttachemnt(ta, angle):
     await ta.move_D_angle(angle=angle, speed_percentage=20)
 
+
 # line nine
 def run_Map_Reveal(td, ta):
     print("start run")
     td.set_speed_percentage(100)
-    td.straight_drive(725)
+    td.straight_drive(720)
     td.set_speed_percentage(7)
     td.turn(-60)
     td.set_speed_percentage(35)
@@ -18,15 +19,16 @@ def run_Map_Reveal(td, ta):
     td.set_speed_percentage(100)
     td.straight_drive(-100)
     td.set_speed_percentage(50)
-    td.straight_drive(315)
+    td.straight_drive(310)
     td.curve(45, -60)
     td.straight_drive(-5)
     run_task(ta.move_C_angle(-300))
     td.straight_drive(60)
     run_task(ta.move_C_angle(200))
-    td_straight_drive(-60)
+    td.stop
+    td_straight_drive(-100)
 
-   
+
 if __name__ == "__main__":
     print("Hello")
     td = TurtleDrive()
