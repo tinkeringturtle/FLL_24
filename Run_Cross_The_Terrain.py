c3db64d3de9b62c20629a6a7270b5792c4717627
Anshi Singh <email.anshi.s@gmail.com>
2025-09-14 14:27:07 -0700

    worked on trying to gey the fruits

--- Run_Cross_The_Terrain.py
+++ Run_Cross_The_Terrain.py
@@ -1,13 +1,17 @@
 from TurtleDrive import *
 from TurtleAttachement import *
-
+async def runAttachemnt(ta, angle):
+    await ta.move_D_angle(angle=angle, speed_percentage=20)
 #11th line from right
 # line nine
 def run_Anshi(td, ta):
     print("start run")
     td.straight_drive(-100)
     td.curve(-175, 115, wait=True)
-    td.straight_drive(-200)
+    td.straight_drive(-475)
+    td.turn(120)
+    td.straight_drive(-70)
+    
 
 
 if __name__ == "__main__":
@@ -15,3 +19,4 @@ if __name__ == "__main__":
     td = TurtleDrive()
     ta = TurtleAttachment()
     run_Anshi(td, ta)
+#run_task(ta.move_C_angle(200))
