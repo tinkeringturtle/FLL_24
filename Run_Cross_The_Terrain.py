8018286421706e3d8262200f4bab7132e30aa038
Anshi Singh <email.anshi.s@gmail.com>
2025-09-21 14:12:41 -0700

    going to go ukd rakc and poin

--- Run_Cross_The_Terrain.py
+++ Run_Cross_The_Terrain.py
@@ -7,10 +7,13 @@ async def runAttachemnt(ta, angle):
 def run_Anshi(td, ta):
     print("start run")
     td.straight_drive(-100)
-    td.curve(-175, 115, wait=True)
-    td.straight_drive(-475)
-    td.turn(120)
-    td.straight_drive(-70)
+    td.curve(-155, 110 )
+    td.straight_drive(-220)
+    td.turn(50)
+    td.straight_drive(60)
+    td.turn(20)
+    td.straight_drive(15)
+    #td.straight_drive(-70)
     
 
 
