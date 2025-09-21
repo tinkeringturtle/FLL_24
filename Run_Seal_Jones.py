528f881fc347a5e3556ece52d768cbd023b9862e
Meghana Mudunuri <rsnmeghana@gmail.com>
2025-09-21 14:15:48 -0700

    we started coding we are doing a backward thing 9/21/25 2:15 - meghana

--- Run_Seal_Jones.py
+++ Run_Seal_Jones.py
@@ -1,23 +1,44 @@
+# line up is the right side of the second black line
 from TurtleDrive import *
 from TurtleAttachement import *
 
+
 async def runAttachemnt(ta, angle):
     await ta.move_C_angle(angle=angle, speed_percentage=40)
+
 
 # line nine
 def Run_Seal_Jones(td, ta):
     print("start run")
 
-#Run starts here
-td.straight_drive()
+    # Run starts here
 
-#Run ends here
+    td.straight_drive(15)
+    td.turn(90)
 
 
-#ATTACHEMENT MOVEMENT ANGLES
- #run_task(ta.move_D_angle(90))   
- #run_task(runAttachemnt(ta, 180))
-    #run_task(runAttachemnt(ta, -180))
+# Run ends here
+
+# run_task(ta.move_C_angle(-130))
+# td.straight_drive(-100)
+
+
+# ATTACHEMENT MOVEMENT ANGLES
+# run_task(ta.move_D_angle(90))  ----- USE THIS CODE ----------- NEGATIVE = UP.  POSOTIVE = DOWN.
+# run_task(runAttachemnt(ta, 180))
+# run_task(runAttachemnt(ta, -180))
+
+
+# COMMENTED CODE:
+
+# td.straight_drive(500)
+# td.turn(25)
+# td.straight_drive(360)
+# td.turn(90)
+
+# run_task(ta.move_C_angle(-130))
+# td.straight_drive(-100)
+
 
 if __name__ == "__main__":
     print("Hello")
