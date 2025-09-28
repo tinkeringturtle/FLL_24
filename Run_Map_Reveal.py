f186d16d1c9e407959c72aa2304cf7f959a72988
Abigail Pernick <abigailmpernick@gmail.com>
2025-09-28 14:10:24 -0700

    map reveal doing the hook thingy

--- Run_Map_Reveal.py
+++ Run_Map_Reveal.py
@@ -18,10 +18,13 @@ def run_Map_Reveal(td, ta):
     td.turn(15)
     td.straight_drive(250)
     td.straight_drive(-40)
-    td.set_speed_percentage(50)
-    run_task(ta.move_D_angle(-80))
     td.set_speed_percentage(100)
-    td.straight_drive(-85)
+    run_task(ta.move_D_angle(-150))
+    td.set_speed_percentage(100)
+    td.straight_drive(-200)
+    run_task(ta.move_C_angle(350))
+    td.curve(-100,-100)
+    
     
 
 
