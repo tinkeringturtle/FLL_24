30f4b5e2a2bac893275113bf3584c825fe2e3f8a
Anshi Singh <email.anshi.s@gmail.com>
2025-10-05 13:02:08 -0700

    worked on run am

--- Run_Cross_The_Terrain.py
+++ Run_Cross_The_Terrain.py
@@ -2,18 +2,15 @@ from TurtleDrive import *
 from TurtleAttachement import *
 async def runAttachemnt(ta, angle):
     await ta.move_D_angle(angle=angle, speed_percentage=20)
-#11th line from right
-# line nine
+#from the left 12th line 
+
 def run_Anshi(td, ta):
     print("start run")
     td.straight_drive(-100)
-    td.curve(-155, 110 )
-    td.straight_drive(-220)
-    td.turn(50)
-    td.straight_drive(60)
-    td.turn(20)
-    td.straight_drive(15)
-    #td.straight_drive(-70)
+    td.turn(-90)
+    td.straight_drive(-525)
+    td.turn(90)
+    td.straight_drive(30)
     
 
 
