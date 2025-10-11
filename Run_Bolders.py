f15067206565039472a336f3d4891a2cebc5d8a3
Emma Hunter <emma.yasuda@gmail.com>
2025-10-11 09:59:32 -0700

    10/11/25 am #2

--- Run_Bolders.py
+++ Run_Bolders.py
@@ -5,38 +5,41 @@ from TurtleAttachement import *
 # line nine
 def run_bolders(td, ta):
     print("start run")
-    td.straight_drive(710) #725
+    td.straight_drive(725) #725
     td.turn(-60)
-    td.straight_drive(25)
-    td.turn(35)
     td.straight_drive(30)
+    td.turn(-35)
+    td.straight_drive(50)
     td.turn(90)
     td.straight_drive(-90)
     td.turn(-90)
     td.set_speed_percentage(20)
     td.straight_drive(-260)
     td.set_speed_percentage(75)
-    td.straight_drive(220)
-    td.turn(-90)
-    td.straight_drive(750)
+    td.straight_drive(160)
+    td.turn(-45)
+    td.straight_drive(370)
+    td.straight_drive(-100)
+    td.turn(-70)
+    td.straight_drive(450)
 
 def run_market(td, ta):
-    td.straight_drive(250)
+    td.straight_drive(240)
     td.turn(-48)
-    td.straight_drive(250)
+    td.straight_drive(270)
     run_task(ta.move_C_angle(180))
     #td.straight_drive(40)
     td.straight_drive(-150)
     td.straight_drive(100)
     run_task(ta.move_C_angle(-180))
-    td.straight_drive(-150)
-    run_task(ta.move_C_angle(100))
+    td.straight_drive(-300)
+   
 
 
 if __name__ == "__main__":
     print("Hello")
     td = TurtleDrive()
     ta = TurtleAttachment()
-    #run_bolders(td, ta)
-    run_market(td, ta)
+    run_bolders(td, ta)
+    #run_market(td, ta)
 
