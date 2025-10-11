578881f14c2a9086d7f1b842b80d0ed0b6c365f2
Anshi Singh <email.anshi.s@gmail.com>
2025-10-11 08:14:33 -0700

    get to spiny bucket trident thing

--- Run_Cross_The_Terrain.py
+++ Run_Cross_The_Terrain.py
@@ -1,17 +1,19 @@
 from TurtleDrive import *
 from TurtleAttachement import *
 async def runAttachemnt(ta, angle):
-    await ta.move_D_angle(angle=angle, speed_percentage=20)
+    await ta.move_D_angle(angle=angle, speed_percentage=100)
 #from the left 12th line 
 
 def run_Anshi(td, ta):
     print("start run")
-    td.straight_drive(-100)
+    td.straight_drive(-120)
     td.turn(-90)
-    td.straight_drive(-525)
+    td.straight_drive(-710)
     td.turn(90)
-    td.straight_drive(30)
-    
+    td.straight_drive(100)
+    run_task(ta.move_D_angle(-1000))
+    td.straight_drive(-100)
+   
 
 
 if __name__ == "__main__":
