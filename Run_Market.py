d01674ebfee196f025fca78ec929b6f1fa5e740a
Emma Hunter <emma.yasuda@gmail.com>
2025-10-26 18:06:22 -0700

    fixed indent

--- Run_Market.py
+++ Run_Market.py
@@ -7,7 +7,7 @@ from pybricks.tools import wait, multitask, run_task
 
 def run_market(td, ta):
     td.straight_drive(220)
-    '''
+
     td.turn(-50)
     td.straight_drive(280)
     run_task(ta.move_C_angle(200))
@@ -28,13 +28,13 @@ def run_market(td, ta):
     ta.move_D_time(speed_percentage=100, time_millisec=1000)
     ta.move_D_time(speed_percentage=-100, time_millisec=800)
     ta.move_D_time(speed_percentage=100, time_millisec=1500)
-   '''
+
   
    
 
 
-    if __name__ == "__main__":
-        print("Hello")
+if __name__ == "__main__":
+    print("Hello")
     td = TurtleDrive()
     ta = TurtleAttachment()
     run_market(td, ta)
