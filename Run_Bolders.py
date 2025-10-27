a78b4fa1a53b940442fbfd1d965a2d4238f4513c
Elly Druru <elly.drury@gmail.com>
2025-10-26 17:35:57 -0700

    removed extra program

--- Run_Bolders.py
+++ Run_Bolders.py
@@ -24,38 +24,12 @@ def run_bolders(td, ta):
     td.turn(-70)
     td.straight_drive(450)
 
-def run_market(td, ta):
-    td.straight_drive(220)
-    '''
-    td.turn(-50)
-    td.straight_drive(280)
-    run_task(ta.move_C_angle(200))
-    
-    td.straight_drive(40)
-    td.straight_drive(-190)
-    
-    td.straight_drive(120)
-    run_task(ta.move_C_angle(-200))
-    td.straight_drive(-300)
 
-    td.straight_drive(420) 
-    ta.move_D_time(speed_percentage=-100, time_millisec=800)
-    ta.move_D_time(speed_percentage=100, time_millisec=1000)
-    ta.move_D_time(speed_percentage=-100, time_millisec=800)
-    ta.move_D_time(speed_percentage=100, time_millisec=1000)
-    ta.move_D_time(speed_percentage=-100, time_millisec=800)
-    ta.move_D_time(speed_percentage=100, time_millisec=1000)
-    ta.move_D_time(speed_percentage=-100, time_millisec=800)
-    ta.move_D_time(speed_percentage=100, time_millisec=1500)
-   '''
-  
-   
 
 
     if __name__ == "__main__":
         print("Hello")
     td = TurtleDrive()
     ta = TurtleAttachment()
-    #run_bolders(td, ta)
-    run_market(td, ta)
+    run_bolders(td, ta)
 
