226b891ee7d1fb708465794b688471ff4f0ca713
Abigail Pernick <abigailmpernick@gmail.com>
2025-10-26 18:00:05 -0700

    6:00

--- Run_Shipreck.py
+++ Run_Shipreck.py
@@ -7,11 +7,19 @@ def run_Shipreck(td, ta):
     print("start run")
     td.set_speed_percentage(100)
     run_task(ta.move_D_angle(100))
-    td.straight_drive(535)
+    td.straight_drive(540)
     run_task(ta.move_D_angle(-105))
-    td.straight_drive(-25)
+    td.straight_drive(-40)
     run_task(ta.move_D_angle(300))
-    td.drive_straight(350)
+    td.straight_drive(-100)
+    td.turn(-15.25)
+    td.straight_drive(175)
+    td.turn(20)
+    td.set_speed_percentage(100)
+    td.straight_drive(420)
+    td.turn(-8)
+    td.straight_drive(-600)
+
 
 
 if __name__ == "__main__":
