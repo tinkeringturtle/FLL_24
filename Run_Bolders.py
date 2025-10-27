f707e729e32b35522872bb5102d2c57abedc4bce
Elly Druru <elly.drury@gmail.com>
2025-10-26 17:33:22 -0700

    split into 2 programs

--- Run_Bolders.py
+++ Run_Bolders.py
@@ -25,8 +25,8 @@ def run_bolders(td, ta):
     td.straight_drive(450)
 
 def run_market(td, ta):
-    
     td.straight_drive(220)
+    '''
     td.turn(-50)
     td.straight_drive(280)
     run_task(ta.move_C_angle(200))
@@ -37,9 +37,7 @@ def run_market(td, ta):
     td.straight_drive(120)
     run_task(ta.move_C_angle(-200))
     td.straight_drive(-300)
-    
-    
-    wait(2)
+
     td.straight_drive(420) 
     ta.move_D_time(speed_percentage=-100, time_millisec=800)
     ta.move_D_time(speed_percentage=100, time_millisec=1000)
@@ -49,7 +47,7 @@ def run_market(td, ta):
     ta.move_D_time(speed_percentage=100, time_millisec=1000)
     ta.move_D_time(speed_percentage=-100, time_millisec=800)
     ta.move_D_time(speed_percentage=100, time_millisec=1500)
-   
+   '''
   
    
 
