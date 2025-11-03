fc3113e748019e7df0d1dde909c6acea48792437
Abigail Pernick <abigailmpernick@gmail.com>
2025-11-02 19:13:07 -0800

    made it turn less

--- Run_Shipreck.py
+++ Run_Shipreck.py
@@ -12,7 +12,7 @@ def run_Shipreck(td, ta):
     td.straight_drive(-40)
     run_task(ta.move_D_angle(300))
     td.straight_drive(-100)
-    td.turn(190)
+    td.turn(184)
     td.straight_drive(-150)
 
 
