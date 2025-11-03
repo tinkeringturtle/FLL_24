f501de700c465512f5d656b0c3e358691d2c8238
Tinkering Turtle <mail.tinkering.turtle@gmail.com>
2025-11-03 15:29:51 -0800

    changes from Nov 2

--- Run_Cross_The_Terrain.py
+++ Run_Cross_The_Terrain.py
@@ -1,22 +1,26 @@
 from TurtleDrive import *
 from TurtleAttachement import *
+
+
 async def runAttachemnt(ta, angle):
     await ta.move_D_angle(angle=angle, speed_percentage=100)
-#from the left 12th line 
+
+
+# from the left 12th line
+
 
 def run_Anshi(td, ta):
     print("start run")
-    td.straight_drive(-130)
+    td.straight_drive(-140)
     td.turn(-90)
     td.straight_drive(-700)
     td.turn(95)
-    td.straight_drive(160)
+    td.straight_drive(175)
     run_task(ta.move_D_angle(-1000))
-    td.set_speed_percentage(55)
-    td.straight_drive(-185)
+    # td.curve_turn(3,4)
     td.straight_drive(160)
-    td.straight_drive(20)
-    td.curve(-80,100)
+    # td.straight_drive(20)
+    # td.curve(-80,100)
 
 
 if __name__ == "__main__":
@@ -24,4 +28,4 @@ if __name__ == "__main__":
     td = TurtleDrive()
     ta = TurtleAttachment()
     run_Anshi(td, ta)
-#run_task(ta.move_C_angle(200))
+# run_task(ta.move_C_angle(200))
--- Run_Market.py
+++ Run_Market.py
@@ -3,34 +3,30 @@ from TurtleAttachement import *
 from pybricks.tools import wait, multitask, run_task
 
 
-
-
 def run_market(td, ta):
     td.straight_drive(220)
 
     td.turn(-50)
-    td.straight_drive(280)
-    run_task(ta.move_C_angle(200))
-    
-    td.straight_drive(40)
-    td.straight_drive(-190)
-    
-    td.straight_drive(120)
-    run_task(ta.move_C_angle(-200))
-    td.straight_drive(-300)
+    # td.straight_drive(280)
+    # run_task(ta.move_C_angle(200))
 
-    td.straight_drive(420) 
-    ta.move_D_time(speed_percentage=-100, time_millisec=800)
-    ta.move_D_time(speed_percentage=100, time_millisec=1000)
-    ta.move_D_time(speed_percentage=-100, time_millisec=800)
-    ta.move_D_time(speed_percentage=100, time_millisec=1000)
-    ta.move_D_time(speed_percentage=-100, time_millisec=800)
-    ta.move_D_time(speed_percentage=100, time_millisec=1000)
-    ta.move_D_time(speed_percentage=-100, time_millisec=800)
-    ta.move_D_time(speed_percentage=100, time_millisec=1500)
+    # td.straight_drive(40)
+    # td.straight_drive(-190)
 
-  
-   
+    # td.straight_drive(120)
+    # run_task(ta.move_C_angle(-200))
+    # td.straight_drive(-300)
+    # td.stop(200)
+
+    # td.straight_drive(420)
+    # ta.move_D_time(speed_percentage=-100, time_millisec=800)
+    # ta.move_D_time(speed_percentage=100, time_millisec=1000)
+    # ta.move_D_time(speed_percentage=-100, time_millisec=800)
+    # ta.move_D_time(speed_percentage=100, time_millisec=1000)
+    # ta.move_D_time(speed_percentage=-100, time_millisec=800)
+    # ta.move_D_time(speed_percentage=100, time_millisec=1000)
+    # ta.move_D_time(speed_percentage=-100, time_millisec=800)
+    # ta.move_D_time(speed_percentage=100, time_millisec=1500)
 
 
 if __name__ == "__main__":
@@ -38,4 +34,3 @@ if __name__ == "__main__":
     td = TurtleDrive()
     ta = TurtleAttachment()
     run_market(td, ta)
-
