997b8d8961a7f7f2fa97e92ec1bffba23ddae427
Anshi <anshisingh@gmail.com>
2025-11-09 18:48:58 -0800

    Finally working changed line up, running into a diffent mission mi=odle befour

--- Run_Cross_The_Terrain.py
+++ Run_Cross_The_Terrain.py
@@ -3,26 +3,27 @@ from TurtleAttachement import *
 
 
 async def runAttachemnt(ta, angle):
-    await ta.move_D_angle(angle=angle, speed_percentage=100)
-    
+    await ta.move_D_angle(angle=angle, speed_percentage=95)
+
 
 # from the left 12th line
 
 
 def run_Anshi(td, ta):
     print("start run")
-    td.straight_drive(-140)
+    td.straight_drive(-150)
     td.turn(-90)
-    td.straight_drive(-700)
+    td.straight_drive(-715)
     td.turn(95)
-    td.straight_drive(170)
+    td.straight_drive(200)
     run_task(ta.move_D_angle(-1000))
-    # td.curve_turn(3,4)
-    td.straight_drive(-160)
-    td.turn(5)
-    run_task(ta.move_C_angle(-30 ))
-    # td.straight_drive(20)
-    # td.curve(-80,100)
+    td.turn(-10)
+    td.straight_drive(-230)
+    run_task(ta.move_C_angle(300))
+    run_task(ta.move_C_angle(-300))
+    td.straight_drive(10)
+    td.turn(90)
+    td.straight_drive(800)
 
 
 if __name__ == "__main__":
