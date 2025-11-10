60fd6c017cde040240a91903c2ca5629a954d8bb
Tinkering Turtle <mail.tinkering.turtle@gmail.com>
2025-11-09 19:52:15 -0800

    all done

--- Run_Shipreck.py
+++ Run_Shipreck.py
@@ -7,9 +7,11 @@ def run_Shipreck(td, ta):
     print("start run")
     td.set_speed_percentage(75)
     td.straight_drive(700)
-    run_task(ta.move_C_angle(75))
-    td.straight_drive(-120)
+    td.set_speed_percentage(10)
+    run_task(ta.move_C_angle(20))
     td.set_speed_percentage(100)
+    td.straight_drive(-120)
+    td.straight_drive(-725)
 
 
 if __name__ == "__main__":
