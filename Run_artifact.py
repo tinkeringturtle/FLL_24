6f2c9c1c7258086873952873583ae76abcabada0
Anshi <anshisingh@gmail.com>
2025-11-23 12:05:03 -0800

    worked on master code blue

--- Run_artifact.py
+++ Run_artifact.py
@@ -3,20 +3,12 @@ from TurtleDrive import *
 from TurtleAttachement import *
 
 
-def set_speed_percentage(
-    self,
-    speed_percentage=DEFAULT_SPEED_PERCENTAGE,
-    acceleration_percentage=DEFAULT_ACCELERATION_PERCENTAGE,
-    turn_rate_percentage=DEFAULT_TURN_RATE_PERCENTAGE,
-    turn_acceleration_percentage=DEFAULT_TURN_ACCELERATION_PERCENTAGE,
-):
-
-    async def runAttachemnt(ta, angle):
-        await ta.move_C_angle(angle=angle, speed_percentage=20)
+async def runAttachemnt(ta, angle):
+    await ta.move_C_angle(angle=angle, speed_percentage=20)
 
 
 # line nine
-def Run_artifact(td, ta):
+def Run_Alice(td, ta):
     print("start run")
 
     # Run starts here
@@ -28,7 +20,7 @@ def Run_artifact(td, ta):
     td.set_speed_percentage(10)
     td.straight_drive(145)  # drive into mineshaft
     wait(2)
-    set_speed_percentage(1)
+    td.set_speed_percentage(1)
     run_task(ta.move_D_angle(-110))  # pick up artifact
     run_task(ta.move_C_angle(-135))  # lift up cart onto other side
     td.straight_drive(-130)  # drive back out
@@ -70,4 +62,4 @@ if __name__ == "__main__":
     )
     td = TurtleDrive()
     ta = TurtleAttachment()
-    Run_artifact(td, ta)
+    Run_Alice(td, ta)
