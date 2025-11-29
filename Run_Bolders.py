47bc87822a987ac01eb7f42a39c161e01f443a55
Meghana Mudunuri <rsnmeghana@gmail.com>
2025-11-29 13:57:17 -0800

    changed boulers because it was trash now its better, emma you sold

--- Run_Bolders.py
+++ Run_Bolders.py
@@ -9,20 +9,24 @@ def run_bolders(td, ta):
     td.straight_drive(725)  # 725
     td.turn(-60)
     td.straight_drive(30)  # boulders
-    td.turn(-35)
+    td.turn(-32)
     td.straight_drive(77)  # market thing
     td.turn(90)
     td.straight_drive(-90)
     td.turn(-93)  # going for boulders angle
     td.set_speed_percentage(20)
-    td.straight_drive(-270)  # going in for boulders
-    td.set_speed_percentage(75)
-    td.straight_drive(160)  # coming out
-    td.turn(-51)  # change for the lever hting
-    td.straight_drive(370)  # hit the lever
-    td.straight_drive(-100)
-    td.turn(-70)  # back up
-    td.straight_drive(470)  # going home
+    td.straight_drive(
+        -320
+    )  # going in for boulders, we also need to go all the way back here, also idk why the code looks like that i cant do anythign about it
+    td.set_speed_percentage(100)
+    td.straight_drive(-160)
+    # td.set_speed_percentage(75)
+    # td.straight_drive(160)  # coming out
+    # td.turn(-51)  # change for the lever hting
+    # td.straight_drive(370)  # hit the lever
+    # td.straight_drive(-100)
+    # td.turn(-70)  # back up
+    # td.straight_drive(470)  # going home
 
 
 if __name__ == "__main__":
--- Run_Cross_The_Terrain.py
+++ Run_Cross_The_Terrain.py
@@ -17,6 +17,7 @@ def run_Anshi(td, ta):
     td.turn(95)
     td.straight_drive(200)
     run_task(ta.move_D_angle(-900))
+    td.turn(-10)
     td.straight_drive(-230)
     run_task(ta.move_C_angle(300))
     run_task(ta.move_C_angle(-300))
