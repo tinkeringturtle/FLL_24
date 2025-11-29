0eced8ac6c2abf765bb0ce6bf951cb65eac1b43b
Meghana Mudunuri <rsnmeghana@gmail.com>
2025-11-29 14:27:54 -0800

    fixed market becuse it wasnt working

--- Run_Market.py
+++ Run_Market.py
@@ -5,22 +5,29 @@ from pybricks.tools import wait, multitask, run_task
 
 # line up left side second dark line from left
 def run_market(td, ta):
-    td.straight_drive(430)
-    ta.move_D_time(speed_percentage=-100, time_millisec=800)
-    ta.move_D_time(speed_percentage=100, time_millisec=1000)
-    ta.move_D_time(speed_percentage=-100, time_millisec=800)
-    ta.move_D_time(speed_percentage=100, time_millisec=1000)
-    ta.move_D_time(speed_percentage=-100, time_millisec=1000)
-    ta.move_D_time(speed_percentage=100, time_millisec=1000)
-    ta.move_D_time(speed_percentage=-100, time_millisec=1000)
-    ta.move_D_time(speed_percentage=100, time_millisec=1500)
+    td.straight_drive(430)  # next code is all about the silo
+    ta.move_D_time(speed_percentage=-100, time_millisec=800)  # 1 hit
+    ta.move_D_time(speed_percentage=100, time_millisec=1000)  # 1 hit
+    ta.move_D_time(speed_percentage=-100, time_millisec=800)  # 2 hit
+    ta.move_D_time(speed_percentage=100, time_millisec=1000)  # 2 hit
+    ta.move_D_time(speed_percentage=-100, time_millisec=1000)  # 3 hit
+    ta.move_D_time(speed_percentage=100, time_millisec=1000)  # 3 hit
+    ta.move_D_time(speed_percentage=-100, time_millisec=1000)  # 4 hit
+    ta.move_D_time(speed_percentage=100, time_millisec=1500)  # 4 hit
     td.straight_drive(-290)
     td.turn(-50)
     td.straight_drive(400)
-    run_task(ta.move_C_angle(200))
-    td.straight_drive(-120)
-    run_task(ta.move_C_angle(-200))
-    td.straight_drive(-400)
+    run_task(ta.move_C_angle(200))  # dropiing for market
+    td.straight_drive(-140)
+    td.straight_drive(20)
+    run_task(ta.move_C_angle(-200))  # lift
+    td.turn(10)
+    td.straight_drive(-72)  # change to get the right position for the lever
+    run_task(ta.move_C_angle(215))  # dropping for lever
+    td.straight_drive(20)  # the go forward before moving the lever
+    td.turn(-45)  # lever hits, this is where we turn to activate the lever thing
+    td.turn(35)
+    td.straight_drive(-400)  # back home
 
 
 if __name__ == "__main__":
