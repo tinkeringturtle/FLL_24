c978bc5c7f800f832568d1a03cd40e29492bb5a5
Tinkering Turtle <mail.tinkering.turtle@gmail.com>
2025-12-02 17:30:20 -0800

    fixed backup

--- Run_Bolders.py
+++ Run_Bolders.py
@@ -22,11 +22,12 @@ def run_bolders(td, ta):
     ta.move_C_time(speed_percentage=-100, time_millisec=1000)  # 1 hit
     ta.move_C_time(speed_percentage=100, time_millisec=1000)  # 1 hit
     ta.move_C_time(speed_percentage=-100, time_millisec=1000)  # 1 hit
-    # td.straight_drive(-16.7)
-    td.set_speed_percentage(50)
     td.straight_drive(-46.7)
     ta.move_D_time(speed_percentage=-100, time_millisec=500)  # 1 hit
-    td.straight_drive(-266.7)
+    td.straight_drive(-66.7)
+    td.set_speed_percentage(50)
+
+    td.straight_drive(-226.7)
     # td.set_speed_percentage(100)
     # td.straight_drive(-160)
     # td.set_speed_percentage(75)
