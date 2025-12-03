bdc97663e84dfe725eeeb6fb7bda3e0d6bf967c7
Tinkering Turtle <mail.tinkering.turtle@gmail.com>
2025-12-02 17:20:55 -0800

    fixing stuff

--- Run_Bolders.py
+++ Run_Bolders.py
@@ -13,7 +13,7 @@ def run_bolders(td, ta):
     td.straight_drive(77)  # market thing
     td.turn(90)
     td.straight_drive(-75)
-    td.turn(-90)  # going for boulders angle
+    td.turn(-88)  # going for boulders angle
     td.set_speed_percentage(20)
     td.straight_drive(-130)  # going in for boulders
     ta.move_C_time(speed_percentage=100, time_millisec=1000)  # 1 hit
@@ -22,11 +22,11 @@ def run_bolders(td, ta):
     ta.move_C_time(speed_percentage=-100, time_millisec=1000)  # 1 hit
     ta.move_C_time(speed_percentage=100, time_millisec=1000)  # 1 hit
     ta.move_C_time(speed_percentage=-100, time_millisec=1000)  # 1 hit
-    td.straight_drive(-46.7)
+    # td.straight_drive(-16.7)
     td.set_speed_percentage(50)
     td.straight_drive(-86.7)
     ta.move_D_time(speed_percentage=-100, time_millisec=500)  # 1 hit
-    td.straight_drive(-167)
+    td.straight_drive(-196.7)
     # td.set_speed_percentage(100)
     # td.straight_drive(-160)
     # td.set_speed_percentage(75)
