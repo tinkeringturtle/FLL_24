77c8a074b68b1d23a0e00723da9386bfa26f83e4
Tinkering Turtle <mail.tinkering.turtle@gmail.com>
2025-12-19 15:26:23 -0800

    made it go back and forwards

--- Run_Map_Reveal.py
+++ Run_Map_Reveal.py
@@ -10,16 +10,17 @@ async def runAttachemnt(ta, angle):
 def run_Map_Reveal(td, ta):
     print("start run")
     td.set_speed_percentage(85)
-    td.straight_drive(565)  # Go to get brush
-    td.set_speed_percentage(100)
-    run_task(ta.move_C_angle(-160))
-    run_task(ta.move_C_angle(170))
-    td.set_speed_percentage(85)
+    td.straight_drive(575)  # Go to get brush
+    td.straight_drive(-175)
+    td.straight_drive(45)
+    run_task(ta.move_C_angle(230))
+    run_task(ta.move_C_angle(-220))
+    run_task(ta.move_C_angle(230))
     return
-    # run_task(ta.move_D_angle(150))
-    # wait(1.5)
-    # td.set_speed_percentage(100)
-    # td.turn(20)
+    run_task(ta.move_D_angle(150))
+    wait(1.5)
+    td.set_speed_percentage(100)
+    td.turn(20)
     td.straight_drive(305)
     td.turn(-78)
     td.turn(13.5)
