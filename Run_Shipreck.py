8e835390d3331f27446bdb0b46bd1f0a3c481b1b
Tinkering Turtle <mail.tinkering.turtle@gmail.com>
2025-12-19 14:52:38 -0800

    fixed shipreck 2:51PM 12/19/25

--- Run_Shipreck.py
+++ Run_Shipreck.py
@@ -2,16 +2,18 @@ from TurtleDrive import *
 from TurtleAttachement import *
 
 
+# line up at 1st black line
 # line nine
 def run_Shipreck(td, ta):
     print("start run")
-    td.set_speed_percentage(75)
-    td.straight_drive(700)
+    td.set_speed_percentage(66)
+    td.straight_drive(475)
     td.set_speed_percentage(10)
-    run_task(ta.move_C_angle(20))
+    run_task(ta.move_D_angle(-140))
     td.set_speed_percentage(100)
-    td.straight_drive(-120)
-    td.straight_drive(-525)
+    td.straight_drive(-175)
+    run_task(ta.move_D_angle(150))
+    td.straight_drive(-450)
 
 
 if __name__ == "__main__":
