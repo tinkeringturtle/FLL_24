07f1e6e5c77a38ef6d05f3ca196a8f7132482b99
Meghana Mudunuri <rsnmeghana@gmail.com>
2026-01-01 20:24:15 -0800

    need to do indiana jones part

--- Run_Forum_Plus_Flags.py
+++ Run_Forum_Plus_Flags.py
@@ -39,12 +39,12 @@ def Run_Forum_Plus_Flags(td, ta):
     td.straight_drive(275)
     td.curve(6, -55)
     td.straight_drive(375)
-    td.curve(100, 92)
+    td.curve(100, 86)
     # td.turn(90)
     td.straight_drive(260)
     run_task(ta.move_D_angle(495))  # dropping flag
-    td.straight_drive(560)
-    td.turn(-80)
+    td.straight_drive(590)
+    td.turn(-75)
     td.straight_drive(190)
 
 
