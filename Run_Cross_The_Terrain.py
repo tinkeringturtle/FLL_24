00e1cadb88f4a6b9ed37e3e1d853e18ba24246ab
Anshi <anshisingh@gmail.com>
2026-01-06 23:38:35 -0800

    worked on  blue attchemnt

--- Run_Cross_The_Terrain.py
+++ Run_Cross_The_Terrain.py
@@ -40,7 +40,7 @@ def run_Anshi(td, ta):
     # run_task(ta.move_C_angle(150, speed_percentage=50))  # arm goes up; seal goes up
     run_task(ta.move_C_angle(150, speed_percentage=35))  # arm goes up; seal goes up
     td.straight_drive(150)
-    td.turn(-40)
+    td.turn(-42)
     td.straight_drive(-400)  # heads toward anshi area
     td.turn(-15)
     td.straight_drive(-420)
