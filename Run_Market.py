b100df5d78e2dc53fb1039fcf64196d0ced9832f
Tinkering Turtle <mail.tinkering.turtle@gmail.com>
2026-01-20 16:20:44 -0800

    merge

--- Run_Market.py
+++ Run_Market.py
@@ -7,25 +7,8 @@ from pybricks.tools import wait, multitask, run_task
 
 # line up left side second dark line from left
 def run_market(td, ta):
-    td.straight_drive(250)
-    td.turn(-50)
-    td.straight_drive(255)
-    run_task(ta.move_C_angle(200))  # dropiing for market
-    td.set_speed_percentage(20)
-    td.straight_drive(15)
-    td.straight_drive(-150)
-    td.set_speed_percentage(50)
-    td.straight_drive(20)
-    run_task(ta.move_C_angle(-200))  # lift
-    td.straight_drive(-50)
-    # td.turn(10)
-    td.straight_drive(-100)  # change to get the right position for the lever
-    run_task(ta.move_C_angle(180))  # dropping for lever
-    td.straight_drive(85)  # the go forward before moving the lever
-    td.turn(-45)  # lever hits, this is where we turn to activate the lever thing
-    td.turn(35)
-
-    td.straight_drive(-400)
+    td.straight_drive(305)
+    run_task(ta.move_D_angle(-320))
 
 
 # run_task(ta.move_C_angle(-200))
