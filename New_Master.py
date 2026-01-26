1608f5ebbae341230fdb3bfb29814ee4f87e0142
Meghana Mudunuri <rsnmeghana@gmail.com>
2026-01-25 21:39:37 -0800

    fixed master

--- New_Master.py
+++ New_Master.py
@@ -1,7 +1,7 @@
 from pybricks.hubs import PrimeHub
 from pybricks.parameters import Button, Color, Port
 from pybricks.pupdevices import ColorSensor
-from pybricks.tools import wait, run_task
+from pybricks.tools import wait
 from TurtleDrive import *
 from TurtleAttachement import *
 import Run_Cross_The_Terrain, Run_artifact, Run_Bolders
@@ -17,10 +17,12 @@ front_sensor = ColorSensor(Port.F)
 BLACK_SAT_MAX, BLACK_VAL_MAX = 30, 30
 WHITE_SAT_MAX, WHITE_VAL_MIN = 40, 80
 
-RED_HUE_MAX1, RED_HUE_MIN2 = 10, 350
+# RED now h <= 7
+RED_HUE_MAX = 7
 RED_SAT_MIN, RED_VAL_MIN = 60, 40
 
-ORANGE_HUE_MIN, ORANGE_HUE_MAX = 11, 45
+# ORANGE now starts h >= 8
+ORANGE_HUE_MIN, ORANGE_HUE_MAX = 8, 45
 ORANGE_SAT_MIN, ORANGE_VAL_MIN = 50, 40
 
 YELLOW_HUE_MIN, YELLOW_HUE_MAX = 46, 80
@@ -44,18 +46,19 @@ def get_detected_color(color_data):
         return "BLACK"
     if v >= WHITE_VAL_MIN and s <= WHITE_SAT_MAX:
         return "WHITE"
-    if (
-        (h <= RED_HUE_MAX1 or h >= RED_HUE_MIN2)
-        and s >= RED_SAT_MIN
-        and v >= RED_VAL_MIN
-    ):
-        return "RED"
+
+    # --- ORANGE first, strictly above RED ---
     if (
         ORANGE_HUE_MIN <= h <= ORANGE_HUE_MAX
         and s >= ORANGE_SAT_MIN
         and v >= ORANGE_VAL_MIN
     ):
         return "ORANGE"
+
+    # --- RED ---
+    if 0 <= h <= RED_HUE_MAX and s >= RED_SAT_MIN and v >= RED_VAL_MIN:
+        return "RED"
+
     if (
         YELLOW_HUE_MIN <= h <= YELLOW_HUE_MAX
         and s >= YELLOW_SAT_MIN
@@ -76,6 +79,7 @@ def get_detected_color(color_data):
         return "GREEN"
     if BLUE_HUE_MIN <= h <= BLUE_HUE_MAX and s >= BLUE_SAT_MIN and v >= BLUE_VAL_MIN:
         return "BLUE"
+
     return "UNKNOWN"
 
 
@@ -95,69 +99,66 @@ while True:
     detected_color = get_detected_color(color_data)
     buttons = hub.buttons.pressed()
 
-    # DEBUG: see sensor readings
+    # DEBUG: see HSV & detected color
     print("HSV:", color_data, "Detected:", detected_color)
 
-    # RED
-    if detected_color == "RED" and Button.RIGHT in buttons:
-        hub.light.on(Color.RED)
-        td.stop()
-        A_Toast_To_Treasure.run_Elly(td, ta)
-        while Button.RIGHT in hub.buttons.pressed():
-            wait(10)
+    # --- MISSIONS ONLY ON RIGHT BUTTON ---
+    if Button.RIGHT in buttons:
+        if detected_color == "RED":
+            hub.light.on(Color.RED)
+            td.stop()
+            A_Toast_To_Treasure.run_Elly(td, ta)
+            while Button.RIGHT in hub.buttons.pressed():
+                wait(10)
 
-    # YELLOW
-    elif detected_color == "YELLOW" and Button.RIGHT in buttons:
-        hub.light.on(Color.YELLOW)
-        td.stop()
-        Run_Bolders.run_bolders(td, ta)
-        while Button.RIGHT in hub.buttons.pressed():
-            wait(10)
+        elif detected_color == "ORANGE":
+            hub.light.on(Color.ORANGE)
+            td.stop()
+            Run_Forum_Plus_Flags.Run_Forum_Plus_Flags(td, ta)
+            while Button.RIGHT in hub.buttons.pressed():
+                wait(10)
 
-    # WHITE
-    elif detected_color == "WHITE" and Button.LEFT in buttons:
-        hub.light.on(Color.YELLOW)
-        td.stop()
-        Run_Market.run_market(td, ta)
-        while Button.LEFT in hub.buttons.pressed():
-            wait(10)
+        elif detected_color == "YELLOW":
+            hub.light.on(Color.YELLOW)
+            td.stop()
+            Run_Bolders.run_bolders(td, ta)
+            while Button.RIGHT in hub.buttons.pressed():
+                wait(10)
 
-    # GREEN
-    elif detected_color == "GREEN" and Button.RIGHT in buttons:
-        hub.light.on(Color.WHITE)
-        td.stop()
-        Run_Map_Reveal.run_Map_Reveal(td, ta)
-        while Button.RIGHT in hub.buttons.pressed():
-            wait(10)
+        elif detected_color == "WHITE":
+            hub.light.on(Color.YELLOW)
+            td.stop()
+            Run_Market.run_market(td, ta)
+            while Button.RIGHT in hub.buttons.pressed():
+                wait(10)
 
-    # LIGHT GREEN
-    elif detected_color == "LIGHT_GREEN" and Button.RIGHT in buttons:
-        hub.light.on(Color.GREEN)
-        td.stop()
-        Run_Shipreck.run_Shipreck(td, ta)
-        while Button.RIGHT in hub.buttons.pressed():
-            wait(10)
+        elif detected_color == "GREEN":
+            hub.light.on(Color.WHITE)
+            td.stop()
+            Run_Map_Reveal.run_Map_Reveal(td, ta)
+            while Button.RIGHT in hub.buttons.pressed():
+                wait(10)
 
-    # ORANGE
-    elif detected_color == "ORANGE" and Button.RIGHT in buttons:
-        hub.light.on(Color.ORANGE)
-        td.stop()
-        Run_Forum_Plus_Flags.Run_Forum_Plus_Flags(td, ta)
-        while Button.RIGHT in hub.buttons.pressed():
-            wait(10)
+        elif detected_color == "LIGHT_GREEN":
+            hub.light.on(Color.GREEN)
+            td.stop()
+            Run_Shipreck.run_Shipreck(td, ta)
+            while Button.RIGHT in hub.buttons.pressed():
+                wait(10)
 
-    # BLUE
-    elif detected_color == "BLUE" and Button.RIGHT in buttons:
-        hub.light.on(Color.BLUE)
-        td.stop()
-        Run_Cross_The_Terrain.run_Anshi(td, ta)
-        while Button.RIGHT in hub.buttons.pressed():
-            wait(10)
+        elif detected_color == "BLUE":
+            hub.light.on(Color.BLUE)
+            td.stop()
+            Run_Cross_The_Terrain.run_Anshi(td, ta)
+            while Button.RIGHT in hub.buttons.pressed():
+                wait(10)
 
-    # BLACK (auto-start)
-    elif detected_color == "BLACK":
-        hub.light.off()
-        Run_artifact.Run_Alice(td, ta)
+        elif detected_color == "BLACK":
+            hub.light.on(Color.BLACK)
+            td.stop()
+            Run_artifact.Run_Alice(td, ta)
+            while Button.RIGHT in hub.buttons.pressed():
+                wait(10)
 
     else:
         hub.light.off()
