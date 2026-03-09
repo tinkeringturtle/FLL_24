8e70dc73f65a8d9df64ee59c5917c88aca43a0dd
Tinkering Turtle <mail.tinkering.turtle@gmail.com>
2026-03-08 20:00:23 -0700

    worked on blue run , got to Iaian

--- blank.py
+++ blank.py
@@ -1,7 +1,10 @@
 from TurtleDrive import *
 from TurtleAttachement import *
 
+
 # from pybricks.tools import wait, multitask, run_task
+async def runAttachemnt(ta, angle):
+    await ta.move_D_angle(angle=angle, speed_percentage=20)
 
 
 # line nine
@@ -9,8 +12,9 @@ def Run_blank(td, ta):
     print("start run")
     td.straight_drive(200)
     td.curve(520, -70)
-    td.straight_drive(260)
+    td.straight_drive(280)
     td.turn(80)
+    run_task(ta.move_D_angle(300))
 
 
 # Main Function
