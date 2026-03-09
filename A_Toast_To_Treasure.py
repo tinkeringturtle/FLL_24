ac15d0b7cfbe6e9f5fe27e7bf7f224e545b57783
Meghana Mudunuri <rsnmeghana@gmail.com>
2026-03-08 19:58:45 -0700

    adding forum items to angler artifacts work in progress. march 8 after dinner-meghana and alice

--- A_Toast_To_Treasure.py
+++ A_Toast_To_Treasure.py
@@ -14,13 +14,13 @@ async def runAttachment(ta, angle):
 def run_Elly(td, ta):
 
     print("start run")
-    td.straight_drive(480)
+    td.straight_drive(570)
 
     async def runAttachment(ta, angle):
         await ta.move_D_angle(angle=angle, speed_percentage=(100))
 
-    run_task(ta.move_D_angle(555))  # spins Angler Artifact)
-    td.straight_drive(-480)
+    run_task(ta.move_D_angle(-555))  # spins Angler Artifact)
+    td.straight_drive(-100)
 
 
 # MAIN FUNCTION
@@ -29,3 +29,14 @@ if __name__ == "__main__":
     td = TurtleDrive()
     ta = TurtleAttachment()
     run_Elly(td, ta)
+
+
+# this is the old codeeeeee
+"""td.straight_drive(480)
+
+    async def runAttachment(ta, angle):
+        await ta.move_D_angle(angle=angle, speed_percentage=(100))
+
+    run_task(ta.move_D_angle(555))  # spins Angler Artifact)
+    td.straight_drive(-480)
+"""
