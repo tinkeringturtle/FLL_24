e723bd11edcda78b5915ae41879bd078a7eefe3d
Anshi <anshisingh@gmail.com>
2026-03-15 20:00:40 -0700

    the falg and Iana work

--- blank.py
+++ blank.py
@@ -10,6 +10,23 @@ async def runAttachemnt(ta, angle):
 # line nine
 def Run_blank(td, ta):
     print("start run")
+    td.straight_drive(150)
+    td.curve(530, -50)
+    run_task(ta.move_D_angle(-125))  # lower arm down
+    wait(1000)
+    td.straight_drive(380)
+    td.turn(20)
+    run_task(ta.move_D_angle(125))  # pick up Iana
+    td.straight_drive(60)  # turn to get flag in
+    td.straight_drive(-160)  # back up form Iana
+    td.turn(-70)
+    return
+    td.turn(-90)
+    td.straight_drive(445)
+    td.turn(-35)
+    td.straight_drive(100)
+    run_task(ta.move_D_angle(-250))
+    return
     td.straight_drive(200)
     td.curve(530, -70)
     td.straight_drive(260)
