77deb0e66c62433858a25dfa7645201a1f91430a
Anshi <anshisingh@gmail.com>
2026-03-27 16:57:33 -0700

    kidia wokrign

--- hihi.py
+++ hihi.py
@@ -10,18 +10,20 @@ def Run_blank(td, ta):
     print("start run")
     td.straight_drive(400)
     td.curve(300, -50)
-    run_task(ta.move_D_angle(250, speed_percentage=60))
+    run_task(ta.move_D_angle(220, speed_percentage=60))
     td.turn(-15)
     td.straight_drive(420)
-    td.turn(20)
+    td.turn(15)  # turn to get the flag in
     td.straight_drive(-115)  # back up form Iana
-    td.turn(20)
-    td.straight_drive(80)
-    return
-    td.turn(-50)
-    td.straight_drive(345)
-    td.turn(-40)
-    td.straight_drive(160)
+    td.turn(20)  # turn towords form Iana
+    td.straight_drive(80)  # pick up Iana
+    run_task(ta.move_D_angle(-200, speed_percentage=60))
+    td.straight_drive(-100)  # back up form Iana
+    td.turn(-50)  # turn to red side
+    td.straight_drive(280)
+    td.turn(-50)  # turn towrad seal
+    td.set_speed_percentage(50)
+    td.straight_drive(400)
     run_task(ta.move_C_angle(-699, speed_percentage=100))
     td.straight_drive(-100)
     run_task(ta.move_C_angle(699, speed_percentage=100))
