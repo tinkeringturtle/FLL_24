2c14c0a4f19f83f92b889a6c06938a50c6e37395
Anshi <anshisingh@gmail.com>
2026-03-29 18:20:07 -0700

    the seal is working !!!

--- hihi.py
+++ hihi.py
@@ -8,20 +8,44 @@ from TurtleAttachement import *
 # line nine
 def Run_blank(td, ta):
     print("start run")
+    run_task(ta.move_D_angle(150, speed_percentage=60))  # lower arm
+    td.straight_drive(590)
+    td.turn(-65)
+    td.straight_drive(490)
+    td.turn(65)  # get flag in
+    td.straight_drive(40)
+    run_task(ta.move_D_angle(-70, speed_percentage=60))  # pick up Iana
+    td.straight_drive(-110)  # back up form Iana
+    td.turn(-90)
+    td.straight_drive(195)
+    td.turn(-45)  # turn towards seal
+    td.straight_drive(180)
+    td.straight_drive(-20)
+    run_task(ta.move_C_angle(-705, speed_percentage=100))
+    run_task(ta.move_C_angle(705, speed_percentage=100))
+    td.straight_drive(-100)  # go away form seal
+
+    td.turn(45)
+    td.straight_drive(190)
+    td.turn(-90)
+    run_task(ta.move_D_angle(100, speed_percentage=60))
+
+    return
     td.straight_drive(400)
     td.curve(300, -50)
-    run_task(ta.move_D_angle(220, speed_percentage=60))
-    td.turn(-15)
+    run_task(ta.move_D_angle(200, speed_percentage=60))
+    td.turn(-20)
     td.straight_drive(420)
     td.turn(15)  # turn to get the flag in
-    td.straight_drive(-115)  # back up form Iana
+    td.straight_drive(-80)  # back up form Iana
+    return
     td.turn(20)  # turn towords form Iana
     td.straight_drive(80)  # pick up Iana
     run_task(ta.move_D_angle(-200, speed_percentage=60))
     td.straight_drive(-100)  # back up form Iana
     td.turn(-50)  # turn to red side
-    td.straight_drive(280)
-    td.turn(-50)  # turn towrad seal
+    td.straight_drive(260)
+    td.turn(-55)  # turn towards seal
     td.set_speed_percentage(50)
     td.straight_drive(400)
     run_task(ta.move_C_angle(-699, speed_percentage=100))
