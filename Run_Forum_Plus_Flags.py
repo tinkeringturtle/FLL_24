423fb586112131fc0d53ca55c4d1d1e2457e5025
Meghana Mudunuri <rsnmeghana@gmail.com>
2026-03-29 18:10:19 -0700

    Orange flag is being worked on, the master code is not detecting anshi said she will work on it

--- Run_Forum_Plus_Flags.py
+++ Run_Forum_Plus_Flags.py
@@ -35,7 +35,7 @@ def Run_Forum_Plus_Flags(td, ta):
     td.set_speed_percentage(turn_rate_percentage=7)
     td.turn(44)
     td.straight_drive(260)
-    run_task(ta.move_C_angle(-90, speed_percentage=20))  # deliver first flag
+    run_task(ta.move_C_angle(-93, speed_percentage=20))  # deliver first flag
     run_task(ta.move_C_angle(200))  # brign arm bakc
     td.straight_drive(-530)
     td.turn(25)
@@ -43,7 +43,7 @@ def Run_Forum_Plus_Flags(td, ta):
     td.set_speed_percentage(1000)
     td.straight_drive(740)
     td.turn(30)
-    td.straight_drive(1000.67)
+    td.straight_drive(1150.6767)
     # note the rest of the code is for backup incase we need to do 2nd flag + aina. Most likely just make this distnace longer
     # td.curve(500, -48)
     # td.straight_drive(370)
