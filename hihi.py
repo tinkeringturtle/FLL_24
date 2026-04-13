fdc6245600b8958950d561f90c74e0bdbe976ab7
Meghana Mudunuri <rsnmeghana@gmail.com>
2026-04-12 19:55:12 -0700

    Anshi is STILL trying to. fix her run :((

--- hihi.py
+++ hihi.py
@@ -20,7 +20,7 @@ def Run_blank(td, ta):
     td.straight_drive(165)  # 195
     td.turn(-45)  # turn towards seal
     td.straight_drive(195)
-    td.straight_drive(-15)
+    td.straight_drive(-20)
     run_task(ta.move_C_angle(-715, speed_percentage=100))
     run_task(ta.move_C_angle(715, speed_percentage=100))
     td.straight_drive(-130)  # go away form seal
