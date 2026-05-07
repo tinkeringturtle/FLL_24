6acba968338223ce29b628260e87ead1ca1d54a2
Anshi <anshisingh@gmail.com>
2026-05-06 17:28:30 -0700

    fixed red

--- A_Toast_To_Treasure.py
+++ A_Toast_To_Treasure.py
@@ -10,9 +10,9 @@ def run_Elly(td, ta):
 
     print("start run")
     td.set_speed_percentage(75)
-    td.straight_drive(585)
+    td.straight_drive(430)
 
-    run_task(ta.move_D_angle(-550, speed_percentage=1000))
+    run_task(ta.move_D_angle(570, speed_percentage=1000))
 
     td.straight_drive(-550)
     wait(2000)
